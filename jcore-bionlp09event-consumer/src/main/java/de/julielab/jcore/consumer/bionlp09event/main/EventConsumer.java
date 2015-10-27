42036a852bcff0f0412e4318311390a2bb2ed966
matthies <matthies@7ac257a9-6a28-0410-b245-f828558649ed>
2015-10-27 07:39:47 +0000

    git-svn-id: https://www.coling.uni-jena.de/svn/uima-components/trunk/jcore-base@9469 7ac257a9-6a28-0410-b245-f828558649ed

---
+++ jcore-bionlp09event-consumer/src/main/java/de/julielab/jcore/consumer/bionlp09event/main/EventConsumer.java
@@ -0,0 +1,190 @@
+ /**
+  * Copyright (c) 2015, JULIE Lab.
+  * All rights reserved. This program and the accompanying materials 
+  * are made available under the terms of the GNU Lesser General Public License (LGPL) v3.0
+  */
+package de.julielab.jcore.consumer.bionlp09event.main;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Iterator;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.cas.CASRuntimeException;
+import org.apache.uima.cas.Type;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.julielab.jcore.consumer.bionlp09event.utils.DocumentWriter;
+import de.julielab.jcore.consumer.bionlp09event.utils.EntityWriter;
+import de.julielab.jcore.consumer.bionlp09event.utils.EventTriggerWriter;
+import de.julielab.jcore.consumer.bionlp09event.utils.EventWriter;
+import de.julielab.jcore.consumer.bionlp09event.utils.ProteinWriter;
+import de.julielab.jules.types.EventMention;
+import de.julielab.jules.types.pubmed.Header;
+
+public class EventConsumer extends JCasAnnotator_ImplBase {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(EventConsumer.class);
+
+	public static final String DIRECTORY_PARAM = "Directory";
+	public static final String A2_FILE_PARAM = "a2FileString";
+	public static final String BIOEVENT_SERVICE_MODE_PARAM = "bioEventServiceMode";
+
+	@ConfigurationParameter(name=DIRECTORY_PARAM, mandatory=false)
+	private File directory;
+	@ConfigurationParameter(name=BIOEVENT_SERVICE_MODE_PARAM, mandatory = true)
+	private boolean bioEventServiceMode;
+	@ConfigurationParameter(name=A2_FILE_PARAM, mandatory=false)
+	private String a2FileString;
+	File a2File;
+	int id = 1;
+
+	@Override
+	public void initialize(UimaContext aContext) throws ResourceInitializationException {
+		super.initialize(aContext);
+
+		if ((Boolean) aContext.getConfigParameterValue(BIOEVENT_SERVICE_MODE_PARAM) != null)
+			bioEventServiceMode = (Boolean) aContext.getConfigParameterValue(BIOEVENT_SERVICE_MODE_PARAM);
+
+		if (!bioEventServiceMode) {
+			String directoryName = (String) aContext.getConfigParameterValue(DIRECTORY_PARAM);
+			directory = new File(directoryName);
+			if (!directory.exists())
+				directory.mkdir();
+		} else {
+
+			a2FileString = (String) aContext.getConfigParameterValue(A2_FILE_PARAM);
+			if (a2FileString != null) {
+				a2File = new File(a2FileString);
+
+			} else {
+				LOGGER.error("[initialize] a2 file path is null. Exit program");
+				System.exit(-1);
+
+			}
+
+		}
+	}
+
+	public void process(JCas aJCas) throws AnalysisEngineProcessException {
+		CAS cas = aJCas.getCas();
+		String id = getDocumentId(cas);
+		String documentText = cas.getDocumentText();
+		Type eventType = cas.getTypeSystem().getType(
+				EventMention.class.getCanonicalName());
+		try {
+			if (!bioEventServiceMode) {
+				DocumentWriter documentWriter = createDocumentWriter(id);
+				EventWriter eventWriter = createEventWriter(documentText, id);
+				documentWriter.write(cas.getJCas());
+				Iterator eventIterator = cas.getAnnotationIndex(eventType)
+						.iterator();
+				int i = 1;
+				while (eventIterator.hasNext()) {
+					EventMention eventMention = (EventMention) eventIterator
+							.next();
+					eventWriter.writeEvent(eventMention);
+				}
+				documentWriter.close();
+				eventWriter.close();
+			} else {
+
+				EventWriter eventWriter = createEventWriter(documentText,
+						a2File);
+				Iterator eventIterator = cas.getAnnotationIndex(eventType)
+						.iterator();
+				int i = 1;
+				while (eventIterator.hasNext()) {
+					EventMention eventMention = (EventMention) eventIterator
+							.next();
+					eventWriter.writeEvent(eventMention);
+				}
+
+				eventWriter.close();
+			}
+
+		} catch (IOException e) {
+			throw new AnalysisEngineProcessException(e);
+		} catch (CASException e) {
+			throw new AnalysisEngineProcessException(e);
+		}
+	}
+
+	private EventWriter createEventWriter(String documentText, File file) {
+		Writer eventFileWriter = null;
+		EventTriggerWriter triggerWriter = null;
+		try {
+			eventFileWriter = null;
+			triggerWriter = null;
+			eventFileWriter = new BufferedWriter(new FileWriter(a2File));
+			triggerWriter = new EventTriggerWriter(eventFileWriter,
+					documentText);
+		} catch (IOException e) {
+			LOGGER.error("[createEventWriter] " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		return new EventWriter(eventFileWriter, null, triggerWriter, null);
+
+	}
+
+	private EventWriter createEventWriter(String documentText, String id)
+			throws AnalysisEngineProcessException {
+		Writer proteinFileWriter = null;
+		Writer eventFileWriter = null;
+		ProteinWriter proteinWriter = null;
+		EventTriggerWriter triggerWriter = null;
+		EntityWriter entityWriter = null;
+
+		try {
+			proteinFileWriter = new BufferedWriter(new FileWriter(new File(
+					directory, id + ".a1")));
+			eventFileWriter = new BufferedWriter(new FileWriter(new File(
+					directory, id + ".a2")));
+			proteinWriter = new ProteinWriter(proteinFileWriter, documentText);
+			triggerWriter = new EventTriggerWriter(eventFileWriter,
+					documentText);
+			entityWriter = new EntityWriter(eventFileWriter, documentText);
+			return new EventWriter(eventFileWriter, proteinWriter,
+					triggerWriter, entityWriter);
+		} catch (IOException e) {
+			throw new AnalysisEngineProcessException(e);
+		}
+	}
+
+	private DocumentWriter createDocumentWriter(String id) throws IOException {
+		return new DocumentWriter(new BufferedWriter(new FileWriter(new File(
+				directory, id + ".txt"))));
+	}
+
+	private String getDocumentId(CAS cas) {
+		Header header = null;
+		Type headerType = cas.getTypeSystem().getType(
+				Header.class.getCanonicalName());
+		Iterator<org.apache.uima.jcas.tcas.Annotation> iterator;
+		try {
+			iterator = cas.getJCas().getAnnotationIndex(Header.type).iterator();
+
+			header = (Header) iterator.next();
+		} catch (CASRuntimeException e) {
+			e.printStackTrace();
+		} catch (CASException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (header != null)
+			return header.getDocId();
+		return new Integer(id++).toString();
+	}
+}
---
+++ jcore-bionlp09event-consumer/src/main/java/de/julielab/jcore/consumer/bionlp09event/utils/DocumentWriter.java
@@ -0,0 +1,42 @@
+ /**
+  * Copyright (c) 2015, JULIE Lab.
+  * All rights reserved. This program and the accompanying materials 
+  * are made available under the terms of the GNU Lesser General Public License (LGPL) v3.0
+  */
+package de.julielab.jcore.consumer.bionlp09event.utils;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import org.apache.uima.cas.Type;
+import org.apache.uima.jcas.JCas;
+
+import de.julielab.jules.types.AbstractText;
+import de.julielab.jules.types.Title;
+
+public class DocumentWriter {
+
+	private Writer writer;
+
+	public DocumentWriter(Writer writer) {
+		this.writer = writer;
+	}
+
+	public void write(JCas cas) throws IOException {
+//		Type titleType = cas.getTypeSystem().getType(Title.class.getCanonicalName());
+//		Type abstractTextType = cas.getTypeSystem().getType(AbstractText.class.getCanonicalName());
+//		
+//		Title title = (Title) cas.getAnnotationIndex(titleType).iterator().next();
+//		AbstractText abstractText = (AbstractText) cas.getAnnotationIndex(abstractTextType).iterator().next();
+		String documentText = cas.getDocumentText();
+//		
+//		writer.write(documentText.substring(title.getBegin(), title.getEnd())+"\n");
+//		writer.write(documentText.substring(abstractText.getBegin(), abstractText.getEnd())+"\n");
+		writer.write(documentText + "\n");
+	}
+
+	public void close() throws IOException {
+		writer.close();
+	}
+
+}
