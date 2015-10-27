f2ba7944729043ff802cd3059582f7e92cb70f84
matthies <matthies@7ac257a9-6a28-0410-b245-f828558649ed>
2015-10-27 07:40:20 +0000

    git-svn-id: https://www.coling.uni-jena.de/svn/uima-components/trunk/jcore-base@9472 7ac257a9-6a28-0410-b245-f828558649ed

---
+++ jcore-cas2iob-consumer/src/main/java/de/julielab/jcore/consumer/cas2iob/application/XMIToIOBApplication.java
@@ -0,0 +1,136 @@
+/** 
+ * XMIToIOBApplication.java
+ * 
+ * Copyright (c) 2015, JULIE Lab.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the GNU Lesser General Public License (LGPL) v3.0
+ *
+ * Author: faessler
+ * 
+ * Current version: 1.1
+ * Since version:   1.0
+ *
+ * Creation date: 06.09.2007 
+ * 
+ * An UIMA-Pipeline capable of reading all XMI files from a directory and converting the annotations
+ * into IOB format.
+ * 
+ * The iob files are stored in the input directory, named <oldfilename>.iob 
+ **/
+
+/**
+ * 
+ */
+package de.julielab.jcore.consumer.cas2iob.application;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FilenameFilter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.impl.XmiCasDeserializer;
+import org.apache.uima.collection.CasConsumer;
+import org.apache.uima.resource.ResourceSpecifier;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
+import org.apache.uima.util.CasCreationUtils;
+import org.apache.uima.util.XMLInputSource;
+import org.apache.uima.util.XMLParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author faessler
+ * 
+ */
+public class XMIToIOBApplication {
+
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(XMIToIOBApplication.class);
+
+	public static void main(String args[]) throws Exception {
+
+		if (args.length != 3) {
+			System.out
+					.println("Usage: XMIToIOBApplication <XMI directory> <TS descriptor> <consumer descriptor>");
+			System.exit(0);
+		}
+
+		File xmiDir = new File(args[0]);
+		String tsDescriptorName = args[1];
+		String consumerDescriptorName = args[2];
+
+		if (!xmiDir.exists() || !xmiDir.isDirectory()) {
+			LOGGER
+					.error("specified XMI dir does not exist or is not a directory: " + xmiDir);
+			System.exit(-1);
+		}
+
+		LOGGER.info("Reading XMI file...");
+
+		ResourceSpecifier spec;
+		CasConsumer toIOBConsumer;
+
+		// getting xmis in directory
+		File[] xmis = xmiDir.listFiles(new ExtensionFileFilter("xmi"));
+		if (xmis.length < 1) {
+			LOGGER.error("specified directory contains no xmi files");
+		}
+
+		// create new iob folder
+		Path iobPath = Paths.get(xmiDir.getAbsolutePath(),"iob/");
+		File iobDir = new File(iobPath.toString());
+		if (!iobDir.exists()) {
+			iobDir.mkdir();
+		}
+		
+		// read type system descriptor
+		XMLParser xmlParser = UIMAFramework.getXMLParser();
+		TypeSystemDescription tsDesc = xmlParser
+				.parseTypeSystemDescription(new XMLInputSource(tsDescriptorName));
+
+		// creating ToIOBConsumer
+		spec = UIMAFramework.getXMLParser().parseResourceSpecifier(
+				new XMLInputSource(consumerDescriptorName));
+		toIOBConsumer = UIMAFramework.produceCasConsumer(spec);
+
+		// now process all xmis
+		for (int i = 0; i < xmis.length; i++) {
+			File myXMI = xmis[i];
+			System.out.println("Processing file " + myXMI.getAbsolutePath() + ".");
+
+			// reading XMI file and creating corresponding CAS object
+			FileInputStream inStream = new FileInputStream(myXMI);
+			
+			CAS myCAS = CasCreationUtils.createCas(tsDesc, null, null);
+//			System.err.println(myXMI.getAbsolutePath() + ".iob");
+			XmiCasDeserializer.deserialize(inStream, myCAS);
+
+			// reconfigure Cos2IOBConsumer
+			String outFile = Paths.get(iobPath.toString(), myXMI.getName()).toString() + ".iob";
+//			String outFile = myXMI.getAbsolutePath() + ".iob";
+//			String outFile = "/tmp/myiob/" + myXMI.getName() + ".iob";
+			toIOBConsumer.setConfigParameterValue("outFileName", outFile);
+			toIOBConsumer.reconfigure();
+			LOGGER.debug("will write file to: " + outFile);
+
+			// process this xmi
+			toIOBConsumer.processCas(myCAS);
+
+		}
+	}
+}
+
+class ExtensionFileFilter implements FilenameFilter {
+	String ext = ""; // the extension to use, e.g. "xmi"
+
+	public ExtensionFileFilter(String ext) {
+		this.ext = ext;
+	}
+
+	public boolean accept(File f, String s) {
+		return s.toLowerCase().endsWith("." + ext);
+	}
+}
---
+++ jcore-cas2iob-consumer/src/main/java/de/julielab/jcore/consumer/cas2iob/utils/UIMAUtils.java
@@ -0,0 +1,123 @@
+/** 
+ * 
+ * These are utils by Philipp Ogren.
+ * They were taken from:
+ * 
+ * http://cslr.colorado.edu/ClearTK/index.cgi/chrome/site/api/src-html/edu/colorado/cleartk/util/AnnotationRetrieval.html#line.237
+ **/
+
+package de.julielab.jcore.consumer.cas2iob.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+
+/**
+ * TODO this tools should be added to JULES Utils
+ */
+public class UIMAUtils {
+
+	/**
+	 * We initially used the AnnotationIndex.subiterator() but ran into issues that we wanted to
+	 * work around concerning the use of type priorities when the window and windowed annotations
+	 * have the same begin and end offsets. By using our own iterator we can back up to the first
+	 * annotation that has the same begining as the window annotation and avoid having to set type
+	 * priorities.
+	 * 
+	 * @param jCas
+	 * @param windowAnnotation
+	 * @return an FSIterator that is at the correct position
+	 */
+	private static FSIterator initializeWindowCursor(JCas jCas, Annotation windowAnnotation) {
+
+		FSIterator cursor = jCas.getAnnotationIndex().iterator();
+
+		cursor.moveTo(windowAnnotation);
+		while (cursor.isValid() && ((Annotation) cursor.get()).getBegin() >= windowAnnotation.getBegin()) {
+			cursor.moveToPrevious();
+		}
+
+		if (cursor.isValid()) {
+			cursor.moveToNext();
+		} else {
+			cursor.moveToFirst();
+		}
+		return cursor;
+	}
+
+	/**
+	 * This method returns all annotations (in order) that are inside a "window" annotation of a
+	 * particular kind. The functionality provided is similar to using
+	 * AnnotationIndex.subiterator(), however we found the use of type priorities which is
+	 * documented in detail in their javadocs to be distasteful. This method does not require that
+	 * type priorities be set in order to work as expected for the condition where the window
+	 * annotation and the "windowed" annotation have the same size and location.
+	 * 
+	 * @param <T>
+	 *            determines the return type of the method
+	 * @param jCas
+	 *            the current jCas or view
+	 * @param windowAnnotation
+	 *            an annotation that defines a window
+	 * @param cls
+	 *            determines the return type of the method
+	 * @return a list of annotations of type cls that are "inside" the window
+	 * @see AnnotationIndex#subiterator(org.apache.uima.cas.text.AnnotationFS)
+	 */
+	public static <T extends Annotation> List<T> getAnnotations(JCas jCas, Annotation windowAnnotation, Class<T> cls) {
+
+		FSIterator cursor = initializeWindowCursor(jCas, windowAnnotation);
+
+		List<T> annotations = new ArrayList<T>();
+		while (cursor.isValid() && ((Annotation) cursor.get()).getBegin() <= windowAnnotation.getEnd()) {
+			Annotation annotation = (Annotation) cursor.get();
+
+			if (cls.isInstance(annotation) && annotation.getEnd() <= windowAnnotation.getEnd())
+				annotations.add(cls.cast(annotation));
+
+			cursor.moveToNext();
+		}
+		return annotations;
+	}
+	
+	/**
+	 * same as getAnnotations however boundaries for windowAnnotation and cls must exactly match!
+	 * @param <T>
+	 * @param jCas
+	 * @param windowAnnotation
+	 * @param cls
+	 * @return list of annotations of type cls
+	 */
+	public static <T extends Annotation> List<T> getExactAnnotations(JCas jCas, Annotation windowAnnotation, Class<T> cls) {
+		List<T> tmp = getAnnotations(jCas, windowAnnotation, cls);
+		List<T> finalList = new ArrayList<T>();
+		for(T anno:tmp) {
+			//System.out.println("offset: " + anno.getBegin() + " - " + anno.getEnd() + " for  " + windowAnnotation.getBegin() + " - " + windowAnnotation.getEnd());
+			if (hasSameOffset(windowAnnotation,anno)) {
+				finalList.add(anno);
+				//System.out.println("adding: ''" + anno.getCoveredText() + "'' for ''" + windowAnnotation.getCoveredText() + "''");
+			} else {
+				//System.out.println("not adding: " + anno.getCoveredText() + " for " + windowAnnotation.getCoveredText());
+			}
+		}
+		return finalList;
+	}
+	
+	/**
+	 * check whether two annotation have exactly the same offset
+	 * @param a annotation 1
+	 * @param b annotation 2
+	 * @return true or false
+	 */
+	public static boolean hasSameOffset(Annotation a, Annotation b) {
+		if (a.getBegin()==b.getBegin() && a.getEnd() == b.getEnd()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
