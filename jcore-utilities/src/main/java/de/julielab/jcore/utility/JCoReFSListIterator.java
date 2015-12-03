123925cd1b0377eb88346b05ec3896c6fb3c04ab
khituras <chew@gmx.net>
2015-12-03 10:16:39 +0100

    Version 2.0.2-SNAPSHOT. Added the class JCoReFSListIterator that just wraps a list of FeatureStructures into an FSIterator interface.

---
+++ jcore-utilities/src/main/java/de/julielab/jcore/utility/JCoReFSListIterator.java
@@ -0,0 +1,97 @@
+package de.julielab.jcore.utility;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.apache.commons.lang.NotImplementedException;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.cas.FeatureStructure;
+
+/**
+ * An iterator that takes a list of FeatureStructures and returns or navigates
+ * its elements exactly in the order the list defines. Actually, the iterator
+ * operates on the exact list, so changes are write-through. External changes to
+ * the list while iterating may lead to undefined behavior of the iterator.<br>
+ * Since no natural order of the list elements is assumed, the
+ * {@link #moveTo(FeatureStructure)} method is currently not implemented since
+ * it couldn't behave as defined in the contract of <tt>FSIterator</tt>.
+ * 
+ * @author faessler
+ * 
+ */
+public class JCoReFSListIterator<T extends FeatureStructure> implements FSIterator<T> {
+
+	private List<T> fsList;
+	private int pos;
+
+	public JCoReFSListIterator(List<T> list) {
+		fsList = list;
+		if (fsList == null)
+			fsList = Collections.emptyList();
+		pos = -1;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return !fsList.isEmpty() && pos < fsList.size() - 1 && pos + 1 >= 0;
+	}
+
+	@Override
+	public T next() {
+		++pos;
+		if (isValid())
+			return get();
+		return null;
+	}
+
+	@Override
+	public void remove() {
+		fsList.remove(pos);
+	}
+
+	@Override
+	public boolean isValid() {
+		return pos >= 0 && pos < fsList.size();
+	}
+
+	@Override
+	public T get() throws NoSuchElementException {
+		if (pos < 0 || pos >= fsList.size())
+			throw new NoSuchElementException("List size: " + fsList.size() + ", index: " + pos);
+		return fsList.get(pos);
+	}
+
+	@Override
+	public void moveToNext() {
+		++pos;
+	}
+
+	@Override
+	public void moveToPrevious() {
+		--pos;
+
+	}
+
+	@Override
+	public void moveToFirst() {
+		pos = 0;
+	}
+
+	@Override
+	public void moveToLast() {
+		pos = fsList.size() - 1;
+
+	}
+
+	@Override
+	public void moveTo(FeatureStructure fs) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public FSIterator<T> copy() {
+		return new JCoReFSListIterator<>(fsList);
+	}
+
+}
---
+++ jcore-utilities/src/test/java/de/julielab/jcore/utility/JCoReFSListIteratorTest.java
@@ -0,0 +1,169 @@
+package de.julielab.jcore.utility;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+
+import de.julielab.jcore.types.Token;
+
+
+public class JCoReFSListIteratorTest {
+	@Test
+	public void testIterator() throws Exception {
+		JCas jCas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-morpho-syntax-types");
+		List<Token> tokenList = new ArrayList<>();
+		Token t1 = new Token(jCas, 11, 17);
+		Token t2 = new Token(jCas, 5, 10);
+		Token t3 = new Token(jCas, 1, 20);
+		Token t4 = new Token(jCas, 3, 5);
+		tokenList.addAll(Arrays.asList(t1, t2, t3, t4));
+
+		JCoReFSListIterator<Token> it = new JCoReFSListIterator<>(tokenList);
+		Token token;
+		assertTrue(it.hasNext());
+		token = (Token) it.next();
+		assertEquals(t1, token);
+
+		assertTrue(it.hasNext());
+		token = (Token) it.next();
+		assertEquals(t2, token);
+
+		assertTrue(it.hasNext());
+		token = (Token) it.next();
+		assertEquals(t3, token);
+
+		assertTrue(it.hasNext());
+		token = (Token) it.next();
+		assertEquals(t4, token);
+
+		assertFalse(it.hasNext());
+	}
+
+	@Test
+	public void testRemove() throws Exception {
+		JCas jCas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-morpho-syntax-types");
+		List<Token> tokenList = new ArrayList<>();
+		Token t1 = new Token(jCas, 11, 17);
+		Token t2 = new Token(jCas, 5, 10);
+		Token t3 = new Token(jCas, 1, 20);
+		Token t4 = new Token(jCas, 3, 5);
+		tokenList.addAll(Arrays.asList(t1, t2, t3, t4));
+
+		Token token;
+		JCoReFSListIterator<Token> it = new JCoReFSListIterator<>(tokenList);
+		assertTrue(it.hasNext());
+		token = (Token) it.next();
+		assertEquals(t1, token);
+
+		assertTrue(it.hasNext());
+		token = (Token) it.next();
+		assertEquals(t2, token);
+
+		// Remove the second token; the third should move to its former position
+		it.remove();
+		assertTrue(it.isValid());
+		assertEquals(t3, it.get());
+
+		assertTrue(it.hasNext());
+		token = (Token) it.next();
+		assertEquals(t4, token);
+
+		// begin from the start and make sure the second token is gone indeed
+		it.moveToFirst();
+
+		it = new JCoReFSListIterator<>(tokenList);
+		assertTrue(it.hasNext());
+		token = (Token) it.next();
+		assertEquals(t1, token);
+
+		assertTrue(it.hasNext());
+		token = (Token) it.next();
+		assertEquals(t3, token);
+
+		assertTrue(it.hasNext());
+		token = (Token) it.next();
+		assertEquals(t4, token);
+
+		assertFalse(it.hasNext());
+	}
+
+	@Test
+	public void testIteration() throws Exception {
+		JCas jCas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-morpho-syntax-types");
+		List<Token> tokenList = new ArrayList<>();
+		Token t1 = new Token(jCas, 11, 17);
+		Token t2 = new Token(jCas, 5, 10);
+		Token t3 = new Token(jCas, 1, 20);
+		Token t4 = new Token(jCas, 3, 5);
+		tokenList.addAll(Arrays.asList(t1, t2, t3, t4));
+
+		JCoReFSListIterator<Token> it = new JCoReFSListIterator<>(tokenList);
+		int count = 0;
+		while (it.hasNext()) {
+			Token token = (Token) it.next();
+			assertNotNull(token);
+			++count;
+		}
+		assertEquals(4, count);
+	}
+
+	@Test
+	public void testMove() throws Exception {
+		JCas jCas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-morpho-syntax-types");
+		List<Token> tokenList = new ArrayList<>();
+		Token t1 = new Token(jCas, 11, 17);
+		Token t2 = new Token(jCas, 5, 10);
+		Token t3 = new Token(jCas, 1, 20);
+		Token t4 = new Token(jCas, 3, 5);
+		tokenList.addAll(Arrays.asList(t1, t2, t3, t4));
+
+		JCoReFSListIterator<Token> it = new JCoReFSListIterator<>(tokenList);
+		it.next();
+		it.next();
+		assertEquals(t2, it.get());
+
+		it.moveToFirst();
+		assertEquals(t1, it.get());
+
+		it.moveToLast();
+		assertEquals(t4, it.get());
+
+		assertFalse(it.hasNext());
+
+		it.moveToNext();
+		boolean exceptionThrown = false;
+		try {
+			it.get();
+		} catch (NoSuchElementException e) {
+			exceptionThrown = true;
+		}
+		assertTrue(exceptionThrown);
+
+		it.moveToPrevious();
+		assertEquals(t4, it.get());
+
+		it.moveToPrevious();
+		assertEquals(t3, it.get());
+
+		it.moveToFirst();
+		assertEquals(t1, it.get());
+
+		it.moveToPrevious();
+		assertFalse(it.isValid());
+
+		assertTrue(it.hasNext());
+
+		// We interpret "next" as "at the next index"; if we move too far left,
+		// the next element would still be invalid and thus, there is no 'next'
+		// element
+		it.moveToPrevious();
+		assertFalse(it.hasNext());
+	}
+}
