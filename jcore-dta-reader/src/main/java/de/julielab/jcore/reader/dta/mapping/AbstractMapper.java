89395fb3c8dff387778b82380b55209d744d09f9
hellrich <hellrich.j@gmail.com>
2016-05-23 15:39:02 +0200

    allowing missing subclassification

--- jcore-dta-reader/src/main/java/de/julielab/jcore/reader/dta/mapping/AbstractMapper.java
+++ jcore-dta-reader/src/main/java/de/julielab/jcore/reader/dta/mapping/AbstractMapper.java
@@ -27,46 +27,39 @@ public abstract class AbstractMapper {
 		this.classification2class = classification2class;
 		this.defaultClass = defaultClass;
 	}
-	
-	 DocumentClassification getClassification(final JCas jcas,
-	            final String xmlFileName,
-	            final Map<String, String[]> classInfo
-	) throws NoSuchMethodException, SecurityException, InstantiationException, 
-	 IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-	        if (classInfo.containsKey(mainClassification)) {
-	            if (classInfo.get(mainClassification).length != 1)
-	                throw new IllegalArgumentException(
-	                        "More than 1 " + mainClassification
-	                                + " classification in " + xmlFileName);
-	            if (classInfo.get(subClassification) == null)
-	                throw new IllegalArgumentException(
-	                        "No " + subClassification + " in " + xmlFileName);
-	            if (classInfo.get(subClassification).length != 1)
-	                throw new IllegalArgumentException(
-	                        "More than 1 " + subClassification
-	                                + " classification in " + xmlFileName);
-	            final String mainClass = classInfo.get(mainClassification)[0];
-	            final String subClass = classInfo.get(subClassification)[0];
 
-	            Class<? extends DocumentClassification> aClass = classification2class
-	                    .get(mainClass);
-	            if (aClass == null) {
-	                if (defaultClass == null)
-	                    throw new IllegalArgumentException(
-	                            mainClass + " not supported in " + xmlFileName);
-	                else {
-	                    aClass = defaultClass;
-	                }
-	            }
-	            final Constructor<? extends DocumentClassification> constructor = aClass
-	                    .getConstructor(new Class[] { JCas.class });
-	            final DocumentClassification classification = constructor
-	                    .newInstance(jcas);
-	            classification.setClassification(mainClass);
-	            classification.setSubClassification(subClass);
-	            classification.addToIndexes();
-	            return classification;
-	        }
-	        return null;
-	    }
+	DocumentClassification getClassification(final JCas jcas, final String xmlFileName,
+			final Map<String, String[]> classInfo) throws NoSuchMethodException, SecurityException,
+			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		if (classInfo.containsKey(mainClassification)) {
+			if (classInfo.get(mainClassification).length != 1)
+				throw new IllegalArgumentException(
+						"More than 1 " + mainClassification + " classification in " + xmlFileName);
+			final String mainClass = classInfo.get(mainClassification)[0];
+			Class<? extends DocumentClassification> aClass = classification2class.get(mainClass);
+			if (aClass == null) {
+				if (defaultClass == null)
+					throw new IllegalArgumentException(mainClass + " not supported in " + xmlFileName);
+				else {
+					aClass = defaultClass;
+				}
+			}
+			final Constructor<? extends DocumentClassification> constructor = aClass
+					.getConstructor(new Class[] { JCas.class });
+			final DocumentClassification classification = constructor.newInstance(jcas);
+			classification.setClassification(mainClass);
+
+			if (classInfo.get(subClassification) != null) {
+				if (classInfo.get(subClassification).length != 1)
+					throw new IllegalArgumentException(
+							"More than 1 " + subClassification + " classification in " + xmlFileName);
+				final String subClass = classInfo.get(subClassification)[0];
+				classification.setSubClassification(subClass);
+			}
+
+			classification.addToIndexes();
+			return classification;
+		}
+		return null;
+	}
 }
