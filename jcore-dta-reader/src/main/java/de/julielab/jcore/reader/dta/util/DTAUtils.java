d08696b230c15f1c802f9d920298cc0e157e0a1a
hellrich <hellrich.j@gmail.com>
2016-05-23 17:15:32 +0200

    slidingSymetricWindow

--- jcore-dta-reader/src/main/java/de/julielab/jcore/reader/dta/util/DTAUtils.java
+++ jcore-dta-reader/src/main/java/de/julielab/jcore/reader/dta/util/DTAUtils.java
@@ -1,20 +1,44 @@
 package de.julielab.jcore.reader.dta.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.uima.cas.FSIterator;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 
+import de.julielab.jcore.types.Sentence;
+import de.julielab.jcore.types.Token;
 import de.julielab.jcore.types.extensions.dta.DocumentClassification;
 
 public class DTAUtils {
-    public static boolean hasAnyClassification(final JCas jcas, final Class<?>... classes) {
-        final FSIterator<Annotation> it = jcas.getAnnotationIndex(DocumentClassification.type).iterator();
-        while (it.hasNext()) {
-            final DocumentClassification classification = (DocumentClassification) it.next();
-            for (final Class<?> c : classes)
-                if (c.isInstance(classification))
-                    return true;
-        }
-        return false;
-    }
+	public static boolean hasAnyClassification(final JCas jcas, final Class<?>... classes) {
+		final FSIterator<Annotation> it = jcas.getAnnotationIndex(DocumentClassification.type).iterator();
+		while (it.hasNext()) {
+			final DocumentClassification classification = (DocumentClassification) it.next();
+			for (final Class<?> c : classes)
+				if (c.isInstance(classification))
+					return true;
+		}
+		return false;
+	}
+
+	public static List<List<String>> slidingSymetricWindow(final JCas jcas, int windowPerSide) {
+		List<List<String>> list = new ArrayList<>();
+		FSIterator<Annotation> sentences = jcas.getAnnotationIndex(Sentence.type).iterator();
+		while (sentences.hasNext()) {
+			Sentence sentence = (Sentence) sentences.next();
+			List<Token> tokens = JCasUtil.selectCovered(Token.class, sentence);
+			if (tokens.size() < windowPerSide * 2 + 1)
+				continue; // to short
+			for (int i = windowPerSide; i < tokens.size() - windowPerSide; ++i) {
+				List<String> inWindow = new ArrayList<>(windowPerSide * 2 + 1);
+				list.add(inWindow);
+				for (int j = i - windowPerSide; j < i + windowPerSide + 1; ++j)
+					inWindow.add(tokens.get(j).getCoveredText());
+			}
+		}
+		return list;
+	}
 }
--- jcore-dta-reader/src/test/java/de/julielab/jcore/reader/dta/util/DTAUtilsTest.java
+++ jcore-dta-reader/src/test/java/de/julielab/jcore/reader/dta/util/DTAUtilsTest.java
@@ -1,6 +1,11 @@
 package de.julielab.jcore.reader.dta.util;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.uima.jcas.JCas;
 import org.junit.Test;
@@ -11,9 +16,20 @@ import de.julielab.jcore.types.extensions.dta.DTABelletristik;
 public class DTAUtilsTest {
 
     @Test
-    public void test() throws Exception {
+    public void testHasAnyClassification() throws Exception {
         final JCas jcas = DTAFileReaderTest.process(true);
         assertTrue(DTAUtils.hasAnyClassification(jcas, DTABelletristik.class));
+    }
+    
+    @Test
+    public void testSlidingSymetricWindow() throws Exception {
+        final JCas jcas = DTAFileReaderTest.process(true);
+        ArrayList<List<String>> expected = new ArrayList<>();
+        expected.add(Arrays.asList("Alte deutsche Lieder gesammelt von L. A. v. Arnim und Clemens Brentano .".split(" ")));
+        assertEquals(expected, 
+        	DTAUtils.slidingSymetricWindow(jcas, 6));
+        assertEquals(4, 
+            	DTAUtils.slidingSymetricWindow(jcas, 5).size());
     }
 
 }
