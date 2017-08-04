9679f03596480335dc0d8d05ecba8adb9ed8e0ed
Bernd Kampe <pikatech@mail.de>
2017-08-04 10:49:19 +0200

    Implemented functionality and fixed test.

--- jcore-ec-code-ae/src/main/java/de/julielab/jcore/ae/ecn/ECNumberAnnotator.java
+++ jcore-ec-code-ae/src/main/java/de/julielab/jcore/ae/ecn/ECNumberAnnotator.java
@@ -1,10 +1,15 @@
 package de.julielab.jcore.ae.ecn;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import de.julielab.jcore.types.Enzyme;
 
 public class ECNumberAnnotator extends JCasAnnotator_ImplBase {
 
@@ -12,7 +17,20 @@ public class ECNumberAnnotator extends JCasAnnotator_ImplBase {
 	
 	@Override
 	public void process(JCas aJCas) throws AnalysisEngineProcessException {
-		log.info("The method de.julielab.jcore.ae.ecn.ECNumberAnnotator#process(JCas) has to be implemented");
+		String document = aJCas.getDocumentText();
+		
+		Pattern p = Pattern.compile("EC \\d\\.(\\d{1,2}|-{1})\\.(\\d{1,2}|-{1})\\.(\\d{1,3}|-{1})");
+		Matcher m = p.matcher(document);
+		
+		while(m.find()) {
+			Enzyme annotation = new Enzyme(aJCas);
+			annotation.setBegin(m.start());
+			annotation.setEnd(m.end());
+			annotation.setComponentId(this.getClass().getName());
+			String type = document.substring(m.start()+3, m.end());
+			annotation.setSpecificType(type);
+			annotation.addToIndexes();
+		}
 	}
 
 }
--- jcore-ec-code-ae/src/test/java/de/julielab/jcore/ae/ec/ECNumberAnnotatorTest.java
+++ jcore-ec-code-ae/src/test/java/de/julielab/jcore/ae/ec/ECNumberAnnotatorTest.java
@@ -2,6 +2,7 @@ package de.julielab.jcore.ae.ec;
 
 import static org.junit.Assert.*;
 
+import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.JCasFactory;
@@ -14,7 +15,7 @@ import de.julielab.jcore.types.Enzyme;
 
 public class ECNumberAnnotatorTest {
 	@Test
-	public void simpleTest() throws Exception {
+	public void simpleTest() throws UIMAException {
 		// This test delivers a brief text with one EC number. We expect a
 		// single Enzyme annotation for this number with the appropriate values
 		// set.
@@ -25,6 +26,6 @@ public class ECNumberAnnotatorTest {
 		Enzyme enzyme = JCasUtil.selectSingle(jcas, Enzyme.class);
 		assertEquals("3.1.1.6", enzyme.getSpecificType());
 		assertEquals(26, enzyme.getBegin());
-		assertEquals(36, enzyme.getBegin());
+		assertEquals(36, enzyme.getEnd());
 	}
 }
