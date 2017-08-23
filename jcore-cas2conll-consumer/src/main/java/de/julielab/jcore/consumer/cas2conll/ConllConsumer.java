1a6d3cd997f5599b51fc7a2f1ae88375bc032e38
egrygorova <elena.grygorova@uni-jena.de>
2017-08-23 14:34:41 +0200

    replaced header everywhere where the path contains de.julielab.

--- jcore-cas2conll-consumer/src/main/java/de/julielab/jcore/consumer/cas2conll/ConllConsumer.java
+++ jcore-cas2conll-consumer/src/main/java/de/julielab/jcore/consumer/cas2conll/ConllConsumer.java
@@ -1,20 +1,30 @@
-/** 
- * ConllConsumer.java
- * 
- * Copyright (c) 2009, JULIE Lab. 
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0 
- *
- * Author: buyko
- * 
- * Current version: //TODO insert current version number 	
- * Since version:   //TODO insert version number of first appearance of this class
- *
- * Creation date: 13.05.2009 
- * 
- * //TODO insert short description
- **/
+/**
+BSD 2-Clause License
 
+Copyright (c) 2017, JULIE Lab
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
 package de.julielab.jcore.consumer.cas2conll;
 
 import java.io.File;
--- jcore-jnet-ae/src/main/java/de/julielab/jnet/tagger/FeatureSubsetModel.java
+++ jcore-jnet-ae/src/main/java/de/julielab/jnet/tagger/FeatureSubsetModel.java
@@ -1,21 +1,30 @@
-/** 
- * FeatureSubsetModel.java
- * 
- * Copyright (c) 2015, JULIE Lab.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the GNU Lesser General Public License (LGPL) v3.0
- *
- * Author: faessler
- * 
- * Current version: 2.4
- * Since version:   2.2
- *
- * Creation date: 29.01.2007 
- * 
- * This class contains a CRF4 model as well as a properties object "featureConfig". The featureConfig
- * object contains information about the feature subset used with this CRF4 model.
- **/
+/**
+BSD 2-Clause License
 
+Copyright (c) 2017, JULIE Lab
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
 package de.julielab.jnet.tagger;
 
 import java.io.Serializable;
