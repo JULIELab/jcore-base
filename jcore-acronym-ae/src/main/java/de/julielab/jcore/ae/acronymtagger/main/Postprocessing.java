04d7fc8d25bfee6a807bdfb7f8237f514145d112
khituras <chew@gmx.net>
2017-08-29 20:59:12 +0200

    Allowing overlapping abbreviations for also overlapping longforms.

--- jcore-acronym-ae/src/main/java/de/julielab/jcore/ae/acronymtagger/main/Postprocessing.java
+++ jcore-acronym-ae/src/main/java/de/julielab/jcore/ae/acronymtagger/main/Postprocessing.java
@@ -1,6 +1,7 @@
 package de.julielab.jcore.ae.acronymtagger.main;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -17,11 +18,12 @@ import de.julielab.jcore.utility.index.TermGenerators.LongOffsetIndexTermGenerat
 
 public class Postprocessing {
 
+	private static final Comparator<Long> overlapComparator = Comparators.longOverlapComparator();
 	private final static LongOffsetIndexTermGenerator termGenerator = TermGenerators.longOffsetTermGenerator();
 
 	public static void doPostprocessing(JCas jcas) {
 		JCoReTreeMapAnnotationIndex<Long, Abbreviation> acronymIndex = new JCoReTreeMapAnnotationIndex<>(
-				Comparators.longOverlapComparator(), termGenerator, termGenerator);
+				overlapComparator, termGenerator, termGenerator);
 		acronymIndex.index(jcas, Abbreviation.type);
 		unifyForLongestAcronym(jcas, acronymIndex);
 		removeAcronymsOnFullforms(jcas, acronymIndex);
@@ -51,14 +53,21 @@ public class Postprocessing {
 			Stream<Abbreviation> overlappingAbbreviations = acronymIndex.search(a);
 			overlappingAbbreviations.filter(oa -> !oa.equals(a)).forEach(overlappingAbbreviation -> {
 				if (overlappingAbbreviation != null) {
-					int aLength = a.getEnd() - a.getBegin();
-					int overlappingLength = overlappingAbbreviation.getEnd() - overlappingAbbreviation.getBegin();
-					if (aLength > overlappingLength)
-						toRemove.add(overlappingAbbreviation);
-					if (overlappingLength > aLength)
-						toRemove.add(a);
-					// if the length should be equal, we can't decide and don't
-					// do anything
+					AbbreviationLongform aFull = a.getTextReference();
+					AbbreviationLongform oFull = overlappingAbbreviation.getTextReference();
+					// we allow embedded acronyms when the full forms also
+					// overlap
+					if (overlapComparator.compare(termGenerator.asKey(aFull), termGenerator.asKey(oFull)) != 0) {
+						int aLength = a.getEnd() - a.getBegin();
+						int overlappingLength = overlappingAbbreviation.getEnd() - overlappingAbbreviation.getBegin();
+						if (aLength > overlappingLength)
+							toRemove.add(overlappingAbbreviation);
+						if (overlappingLength > aLength)
+							toRemove.add(a);
+						// if the length should be equal, we can't decide and
+						// don't
+						// do anything
+					}
 				}
 			});
 		}
