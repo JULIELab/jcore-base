2d0baadd3480d682a25b3efc9727e2c14457a1c5
khituras <chew@gmx.net>
2017-09-20 21:02:37 +0200

    Added a parameter to avoid the addition of entities from the input documents.

--- jcore-pubtator-reader/src/main/java/de/julielab/jcore/reader/pubtator/PubtatorReader.java
+++ jcore-pubtator-reader/src/main/java/de/julielab/jcore/reader/pubtator/PubtatorReader.java
@@ -5,6 +5,7 @@ import static de.julielab.jcore.reader.pubtator.PubtatorDocument.EMPTY_DOCUMENT;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
 
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
@@ -34,9 +35,12 @@ public class PubtatorReader extends CollectionReader_ImplBase {
 	private static final String COMPONENT_NAME = PubtatorReader.class.getCanonicalName();
 
 	public static final String PARAM_INPUT = "Input";
+	public static final String PARAM_ADD_ENTITIES = "AddEntities";
 
 	@ConfigurationParameter(name = PARAM_INPUT, mandatory = true)
 	private File input;
+	@ConfigurationParameter(name = PARAM_ADD_ENTITIES, mandatory = false)
+	private boolean addEntities;
 
 	private File[] inputFiles;
 	private BufferedReader currentReader;
@@ -50,11 +54,13 @@ public class PubtatorReader extends CollectionReader_ImplBase {
 		super.initialize();
 		String inputDirectoryPath = (String) getConfigParameterValue(PARAM_INPUT);
 		input = new File(inputDirectoryPath);
+		addEntities = (boolean) Optional.ofNullable(getConfigParameterValue(PARAM_ADD_ENTITIES)).orElseGet(() -> false);
 		if (!input.exists())
 			throw new ResourceInitializationException(
 					new IllegalArgumentException("The path " + input.getAbsolutePath() + " does not exist."));
 		if (input.isDirectory())
-			inputFiles = input.listFiles((f, s) -> s.endsWith(".txt") || s.endsWith(".txt.gz") || s.endsWith(".txt.gzip"));
+			inputFiles = input
+					.listFiles((f, s) -> s.endsWith(".txt") || s.endsWith(".txt.gz") || s.endsWith(".txt.gzip"));
 		else
 			inputFiles = new File[] { input };
 		index = 0;
@@ -68,62 +74,66 @@ public class PubtatorReader extends CollectionReader_ImplBase {
 				Header header = new Header(jcas);
 				header.setDocId(currentDocument.getDocumentId());
 				header.addToIndexes();
-				// the offsets of the pubtator entity format expect that title and abstracts are separated by a single character
+				// the offsets of the pubtator entity format expect that title
+				// and abstracts are separated by a single character
 				jcas.setDocumentText(currentDocument.getTitle() + "\n" + currentDocument.getAbstractText());
 				Title title = new Title(jcas, 0, currentDocument.getTitle().length());
 				title.setTitleType("document");
 				title.setComponentId(COMPONENT_NAME);
 				title.addToIndexes();
-				AbstractText abstractText = new AbstractText(jcas, title.getEnd()+1, title.getEnd()+1+currentDocument.getAbstractText().length());
+				AbstractText abstractText = new AbstractText(jcas, title.getEnd() + 1,
+						title.getEnd() + 1 + currentDocument.getAbstractText().length());
 				abstractText.setComponentId(COMPONENT_NAME);
 				abstractText.addToIndexes();
-				
-				for (PubtatorEntity e : currentDocument.getEntities()) {
-					Annotation a;
-					switch (e.getEntityType()) {
-					case "Chemical":
-						Chemical chemical = new Chemical(jcas);
-						chemical.setNameOfSubstance(e.getText());
-						chemical.setRegistryNumber(e.getEntityId());
-						a = chemical;
-						break;
-					case "Disease":
-						a = new Disease(jcas);
-						break;
-					case "Gene":
-						Gene g = new Gene(jcas);
-						GeneResourceEntry geneResourceEntry = new GeneResourceEntry(jcas, e.getBegin(), e.getEnd());
-						geneResourceEntry.setEntryId(e.getEntityId());
-						geneResourceEntry.setSource("NCBI Gene");
-						geneResourceEntry.setComponentId(COMPONENT_NAME);
-						FSArray geneEntryList = new FSArray(jcas, 1);
-						geneEntryList.set(0, geneResourceEntry);
-						g.setResourceEntryList(geneEntryList);
-						a = g;
-						break;
-					case "Species":
-						Organism o = new Organism(jcas);
-						ResourceEntry organismResourceEntry = new GeneResourceEntry(jcas, e.getBegin(), e.getEnd());
-						organismResourceEntry.setEntryId(e.getEntityId());
-						organismResourceEntry.setSource("NCBI Taxonomy");
-						organismResourceEntry.setComponentId(COMPONENT_NAME);
-						FSArray organismEntryList = new FSArray(jcas, 1);
-						organismEntryList.set(0, organismResourceEntry);
-						o.setResourceEntryList(organismEntryList);
-						a = o;
-						break;
-					case "Mutation":
-					default:
-						EntityMention em = new EntityMention(jcas);
-						a = em;
-						break;
+
+				if (addEntities) {
+					for (PubtatorEntity e : currentDocument.getEntities()) {
+						Annotation a;
+						switch (e.getEntityType()) {
+						case "Chemical":
+							Chemical chemical = new Chemical(jcas);
+							chemical.setNameOfSubstance(e.getText());
+							chemical.setRegistryNumber(e.getEntityId());
+							a = chemical;
+							break;
+						case "Disease":
+							a = new Disease(jcas);
+							break;
+						case "Gene":
+							Gene g = new Gene(jcas);
+							GeneResourceEntry geneResourceEntry = new GeneResourceEntry(jcas, e.getBegin(), e.getEnd());
+							geneResourceEntry.setEntryId(e.getEntityId());
+							geneResourceEntry.setSource("NCBI Gene");
+							geneResourceEntry.setComponentId(COMPONENT_NAME);
+							FSArray geneEntryList = new FSArray(jcas, 1);
+							geneEntryList.set(0, geneResourceEntry);
+							g.setResourceEntryList(geneEntryList);
+							a = g;
+							break;
+						case "Species":
+							Organism o = new Organism(jcas);
+							ResourceEntry organismResourceEntry = new GeneResourceEntry(jcas, e.getBegin(), e.getEnd());
+							organismResourceEntry.setEntryId(e.getEntityId());
+							organismResourceEntry.setSource("NCBI Taxonomy");
+							organismResourceEntry.setComponentId(COMPONENT_NAME);
+							FSArray organismEntryList = new FSArray(jcas, 1);
+							organismEntryList.set(0, organismResourceEntry);
+							o.setResourceEntryList(organismEntryList);
+							a = o;
+							break;
+						case "Mutation":
+						default:
+							EntityMention em = new EntityMention(jcas);
+							a = em;
+							break;
+						}
+						if (a instanceof EntityMention)
+							((EntityMention) a).setSpecificType(e.getEntityType());
+						a.setBegin(e.getBegin());
+						a.setEnd(e.getEnd());
+						a.setComponentId(COMPONENT_NAME);
+						a.addToIndexes();
 					}
-					if (a instanceof EntityMention)
-						((EntityMention) a).setSpecificType(e.getEntityType());
-					a.setBegin(e.getBegin());
-					a.setEnd(e.getEnd());
-					a.setComponentId(COMPONENT_NAME);
-					a.addToIndexes();
 				}
 			} catch (CASException e) {
 				throw new CollectionException(e);
