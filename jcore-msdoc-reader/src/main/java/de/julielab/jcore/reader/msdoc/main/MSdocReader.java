fe13f66596f10f97487f6711cbf53b11e0b4bd3c
hellrich <hellrich.j@gmail.com>
2018-02-16 09:44:16 +0100

    :renamed folder :P

---
+++ jcore-msdoc-reader/src/main/java/de/julielab/jcore/reader/msdoc/main/MSdocReader.java
@@ -0,0 +1,210 @@
+/**
+ * MSdocReader.java
+ *
+ * Copyright (c) 2017, JULIE Lab.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the BSD-2-Clause License
+ *
+ * @author Christina Lohr
+ *
+ * Current version: 1.2
+ * Since version:   1.1
+ *
+ * Creation date: 11.04.2017
+ * Update: 17.08.2017
+ *
+ * A UIMA <code>CollectionReader</code> that reads in simple text from a file.
+ * Derived form Apache UIMA example <code>FileSystemCollectionReader</code>.
+ **/
+
+package de.julielab.jcore.reader.msdoc.main;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.collection.CollectionException;
+import org.apache.uima.collection.CollectionReader_ImplBase;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.util.Progress;
+import org.apache.uima.util.ProgressImpl;
+
+import de.julielab.jcore.types.pubmed.Header;
+
+public class MSdocReader extends CollectionReader_ImplBase {
+	public static final String DIRECTORY_INPUT = "InputDirectory";
+	public static final String FILENAME_AS_DOC_ID = "UseFilenameAsDocId";
+	public static final String ALLOWED_FILE_EXTENSIONS = "AllowedFileExtensions";
+	public static final String DIRECTORY_SUBDIRS = "ReadSubDirs";
+
+	private ArrayList<File> files;
+
+	private int fileIndex;
+
+	private File inputDirectory;
+	private boolean useFilenameAsDocId;
+	private boolean useSubDirs;
+
+	/**
+	 * @see org.apache.uima.collection.CollectionReader_ImplBase#initialize()
+	 */
+	@Override
+	public void initialize() throws ResourceInitializationException {
+		inputDirectory = new File(((String) getConfigParameterValue(DIRECTORY_INPUT)).trim());
+
+		Boolean filenameAsDocId = (Boolean) getConfigParameterValue(FILENAME_AS_DOC_ID);
+
+		if (null == filenameAsDocId) {
+			useFilenameAsDocId = false;
+		} else {
+			useFilenameAsDocId = filenameAsDocId;
+		}
+
+		String[] allowedExtensionsArray = (String[]) getConfigParameterValue(ALLOWED_FILE_EXTENSIONS);
+
+		final Set<String> allowedExtensions = new HashSet<>();
+
+		if (null != allowedExtensionsArray) {
+			for (int i = 0; i < allowedExtensionsArray.length; i++) {
+				allowedExtensions.add(allowedExtensionsArray[i]);
+			}
+		}
+
+		Boolean subdir = (Boolean) getConfigParameterValue(DIRECTORY_SUBDIRS);
+
+		if (null == subdir) {
+			useSubDirs = false;
+		} else {
+			useSubDirs = subdir;
+		}
+
+		fileIndex = 0;
+		files = new ArrayList<File>();
+
+		try {
+			createFileListByType(inputDirectory, allowedExtensions);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see org.apache.uima.collection.CollectionReader#hasNext()
+	 */
+	@Override
+	public boolean hasNext() {
+		return fileIndex < files.size();
+	}
+
+	/**
+	 * @see org.apache.uima.collection.CollectionReader#getNext(org.apache.uima.cas.CAS)
+	 */
+	@Override
+	public void getNext(CAS aCAS) throws IOException, CollectionException {
+		JCas jcas;
+		try {
+			jcas = aCAS.getJCas();
+		} catch (CASException e) {
+			throw new CollectionException(e);
+		}
+
+		File file = files.get(fileIndex++);
+
+		ReadSingleMSdoc.INPUT_FILE = file.getPath();
+		ReadSingleMSdoc.doc2Text();
+
+		/**
+		 * Read the doc-file:
+		 * 
+		 * We implemented different ways to convert the *.doc-files, look into
+		 * ReadSingleMSdoc.doc2Text(), this reads the structure of the
+		 * *.doc-file.
+		 */
+
+		// String textOnly = ReadSingleMSdoc.CONTENT_NORMAL;
+		String textWithMarkedTables = ReadSingleMSdoc.CONTENT_TAB_MARKED;
+		// String labParams = ReadSingleMSdoc.LAB_PARAMS_NORMAL;
+		// String textHMTL = ReadSingleMSdoc.CONTENT_HTML;
+
+		jcas.setDocumentText(textWithMarkedTables);
+
+		if (useFilenameAsDocId) {
+			String filename = file.getName();
+			int extDotIndex = filename.lastIndexOf('.');
+
+			if (extDotIndex > 0) {
+				filename = filename.substring(0, extDotIndex);
+			}
+
+			int extUnderScoreIndex = filename.lastIndexOf('_');
+
+			if (extUnderScoreIndex > 0) {
+				filename = filename.substring(0, extUnderScoreIndex);
+			}
+
+			Header header = new Header(jcas);
+			header.setDocId(filename);
+			header.addToIndexes();
+		}
+
+		// writeArtifactIntoTXT(file.getPath(), textOnly);
+//		writeArtifactIntoTXT(file.getPath(), textWithMarkedTables);
+		// writeArtifactIntoTXT(file.getPath(), textHTML);
+
+	}
+
+	/**
+	 * @see org.apache.uima.collection.base_cpm.BaseCollectionReader#close()
+	 */
+	@Override
+	public void close() throws IOException {
+	}
+
+	/**
+	 * @see org.apache.uima.collection.base_cpm.BaseCollectionReader#getProgress()
+	 */
+	@Override
+	public Progress[] getProgress() {
+		return new Progress[] { new ProgressImpl(fileIndex, files.size(), Progress.ENTITIES) };
+	}
+
+	private String[] createFileListByType(File inputDirectory, final Set<String> allowedExtensions) throws IOException {
+		String[] path = new File(inputDirectory.getPath()).list();
+
+		for (int i = 0; i < path.length; i++) {
+			File file = new File(inputDirectory.getAbsolutePath() + "/" + path[i]);
+
+			String CurrentExtension = path[i].substring(path[i].lastIndexOf('.') + 1);
+			if (allowedExtensions.contains(CurrentExtension)) {
+				files.add(file);
+			}
+
+			if (useSubDirs && file.isDirectory()) {
+				createFileListByType(file, allowedExtensions);
+			}
+		}
+
+		return path;
+	}
+
+	private static void writeArtifactIntoTXT(String file, String text) {
+		file = file.substring(0, file.length() - 4) + ".txt";
+
+		File artifactFile = new File(file); // new File(OUTDIR + File.separator
+											// + fileName);
+		try (FileOutputStream outputStream = new FileOutputStream(artifactFile)) {
+			outputStream.write(text.getBytes());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ jcore-msdoc-reader/src/main/java/de/julielab/jcore/reader/msdoc/main/ReadSingleMSdoc.java
@@ -0,0 +1,269 @@
+/**
+ * MSdocReader.java
+ * 
+ * @author Christina Lohr
+ *
+ * Copyright (c) 2017, JULIE Lab.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the BSD-2-Clause License
+ *
+ * Current version: 1.2
+ * Since version:   1.1
+ *
+ * Creation date: 11.04.2017
+ * Update: 17.08.2017
+ * 
+ * There are different ways to read *.doc-files of MS Word and transform it to plain text.
+ * This component was developed for clinical documents containing a separated part of laboratory values.
+ * Tables can be extracted with HTML code by <tables>...</tables>, <tr>...</tr> and <td>...</td>.
+ * Now, only tables are markable with HTML.
+ * Another way to mark tables is the identification by ´|         |´.
+ * These flags are useful for the annotation by BRAT.
+ * 
+ * * CONTENT_HTML            ... Full content, tables marked by HTML.
+   * CONTENT_NORMAL          ... Full content, not marked.
+   * CONTENT_TAB_MARKED      ... Full content, tables marked by |.
+   * LAB_PARAMS_HTML         ... Part of laboratory values, tables marked by HTML.
+   * LAB_PARAMS_NORMAL       ... Part of laboratory values, not marked.
+   * LAB_PARAMS_TAB_MARKED   ... Part of laboratory values, tables marked by |.
+   * CONTENT_WOLP_HTML       ... Full content without laboratory values, tables marked by HTML.
+   * CONTENT_WOLP_NORMAL     ... Full content without laboratory values, not marked.
+   * CONTENT_WOLP_TAB_MARKED ... Full content without laboratory values, tables marked by |.
+ * 
+ * Non-readable characters are removed, see characterChecker().
+ */
+
+package de.julielab.jcore.reader.msdoc.main;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.usermodel.Paragraph;
+import org.apache.poi.hwpf.usermodel.Range;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+
+public class ReadSingleMSdoc {
+
+	public static String INPUT_FILE = "";
+
+	public static String CONTENT_HTML = "";
+	public static String CONTENT_NORMAL = "";
+	public static String CONTENT_TAB_MARKED = "";
+
+	public static String LAB_PARAMS_HTML = "";
+	public static String LAB_PARAMS_NORMAL = "";
+	public static String LAB_PARAMS_TAB_MARKED = "";
+
+	// WOLP... without lab params
+	public static String CONTENT_WOLP_HTML = "";
+	public static String CONTENT_WOLP_NORMAL = "";
+	public static String CONTENT_WOLP_TAB_MARKED = "";
+
+	public static boolean LAB_TABLE = false;
+
+	public static void doc2Text() {
+
+		CONTENT_HTML = "";
+		CONTENT_NORMAL = "";
+		CONTENT_TAB_MARKED = "";
+
+		LAB_PARAMS_HTML = "";
+		LAB_PARAMS_NORMAL = "";
+		LAB_PARAMS_TAB_MARKED = "";
+
+		// WOLP... without lab params
+		CONTENT_WOLP_HTML = "";
+		CONTENT_WOLP_NORMAL = "";
+		CONTENT_WOLP_TAB_MARKED = "";
+
+		try {
+			InputStream fis = new FileInputStream(INPUT_FILE);
+			POIFSFileSystem fs = new POIFSFileSystem(fis);
+
+			@SuppressWarnings("resource")
+			HWPFDocument doc = new HWPFDocument(fs);
+
+			Range docRange = doc.getRange();
+
+			boolean inTable = false;
+			boolean inRow = false;
+
+			boolean tbOpen = false;
+			boolean tdOpen = false;
+
+			for (int i = 0; i < docRange.numParagraphs(); i++) {
+				Paragraph par = docRange.getParagraph(i);
+
+				if (par.isInTable()) {
+					if (!inTable) {
+						CONTENT_HTML += "\n<table>\n";
+						inTable = true;
+					}
+					if (!inRow) {
+						if (tbOpen == true) {
+							CONTENT_HTML += "</tr>\n";
+						}
+
+						CONTENT_HTML += "<tr>";
+						inRow = true;
+						tbOpen = true;
+					}
+					if (par.isTableRowEnd()) {
+						inRow = false;
+					} else {
+						if ((!tdOpen) && (par.text().endsWith("\u0007"))) {
+							CONTENT_HTML += "<td>";
+							CONTENT_HTML += par.text().replaceAll("\\s", " ");
+							CONTENT_HTML += "</td>";
+						} else if ((!tdOpen) && (!(par.text().endsWith("\u0007")))) {
+							CONTENT_HTML += "<td>";
+							CONTENT_HTML += par.text().replaceAll("\\s", " ");
+							tdOpen = true;
+						}
+
+						else if (tdOpen && (par.text().endsWith("\u0007"))) {
+							CONTENT_HTML += par.text().replaceAll("\\s", " ");
+							CONTENT_HTML += "</td>";
+							tdOpen = false;
+						} else if (tdOpen && (!(par.text().endsWith("\u0007")))) {
+							CONTENT_HTML += par.text().replaceAll("\\s", " ");
+						}
+					}
+				} else {
+					if (inTable) {
+						CONTENT_HTML += "</tr>\n</table>\n";
+						inTable = false;
+					}
+					CONTENT_HTML += par.text() + "<br/>";
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		}
+
+		CONTENT_HTML = characterChecker(CONTENT_HTML);
+
+		CONTENT_NORMAL = makeNormalText(CONTENT_HTML);
+		CONTENT_TAB_MARKED = makeTabMarked(CONTENT_HTML);
+
+		// CONTENT_TEXT_ONLY = characterChecker(CONTENT_TEXT_ONLY);
+
+		String[] contentSplitMT = characterChecker(CONTENT_HTML).split("\n");
+		// String[] contentSplitNMT =
+		// characterChecker(CONTENT_TEXT_ONLY).split("\n");
+
+		boolean foundLabParams = false;
+
+		for (int i = 0; i < contentSplitMT.length; i++) {
+
+			if (contentSplitMT[i].contains("<br/>Laborwerte:")) {
+				for (int j = i; j < contentSplitMT.length; j++) {
+					if (contentSplitMT[j].contains("Normwert")) {
+						foundLabParams = true;
+						break;
+					}
+				}
+			}
+
+			if (!foundLabParams) {
+				CONTENT_WOLP_HTML = CONTENT_WOLP_HTML + contentSplitMT[i] + "\n";
+			} else {
+				LAB_PARAMS_HTML = LAB_PARAMS_HTML + contentSplitMT[i] + "\n";
+			}
+		}
+
+		CONTENT_WOLP_NORMAL = makeNormalText(CONTENT_WOLP_HTML);
+		CONTENT_WOLP_TAB_MARKED = makeTabMarked(CONTENT_WOLP_HTML);
+
+		LAB_PARAMS_NORMAL = makeNormalText(LAB_PARAMS_HTML);
+		LAB_PARAMS_TAB_MARKED = makeTabMarked(LAB_PARAMS_HTML);
+
+		if ((LAB_PARAMS_HTML.contains("<table>")) && (LAB_PARAMS_HTML.contains("</table>"))
+				&& (LAB_PARAMS_HTML.contains("<tr>")) && (LAB_PARAMS_HTML.contains("</tr>"))) {
+			LAB_TABLE = true;
+		} else {
+			LAB_TABLE = false;
+		}
+	}
+
+	/**
+	 * The Reader doesn't use all characters.
+	 * 
+	 * Private Use Area from U+E000 until U+F8FF
+	 * https://unicode-table.com/en/blocks/private-use-area/
+	 * 
+	 * Basic Latin from 0020 until 007F, 007F is DEL-character, not used, used
+	 * characters into 0020-007E
+	 * https://unicode-table.com/en/blocks/basic-latin/
+	 * 
+	 * Latin-1 Supplement from 0080 until 00FF used characters into 00A1-00FF
+	 * https://unicode-table.com/en/blocks/latin-1-supplement/
+	 * 
+	 */
+
+	private static String characterChecker(String content) {
+		String output = "";
+
+		for (int i = 0; i < content.length(); i++) {
+			int charvalue = ReadIntegerValueOf1Character(content.charAt(i));
+
+			if ((!((57343 <= charvalue) && (charvalue <= 63742)))
+					&& (((3 <= charvalue) && (charvalue <= 125)) || ((160 <= charvalue) && (charvalue <= 254)))) {
+				output = output + content.charAt(i);
+			}
+		}
+
+		output = output.replaceAll("", "");
+
+		output = output.replaceAll("\u0007", "");
+		output = output.replaceAll("\u0008", "");
+		output = output.replaceAll("\u000B", "");
+		output = output.replaceAll("\u000C", "");
+		output = output.replaceAll("\\r", "\n");
+
+		return output;
+	}
+
+	private static int ReadIntegerValueOf1Character(char c) {
+		String y = c + Character.digit(c, 10) + " ";
+		y = y.substring(0, y.length() - 1);
+		return Integer.parseInt(y);
+	}
+
+	private static String makeNormalText(String input) {
+		String text = input;
+		text = text.replaceAll("<table>", "");
+		text = text.replaceAll("</table>", "");
+		text = text.replaceAll("<tr>", "");
+		text = text.replaceAll("</tr>", "\n");
+		text = text.replaceAll("<td>", "");
+		text = text.replaceAll("</td>", "\t");
+		text = text.replaceAll("<br/>", "");
+		text = text.replaceAll("\n+", "\n");
+
+		if (text.startsWith("\n")) {
+			text = text.replaceFirst("\n", "");
+		}
+
+		return text;
+	}
+
+	private static String makeTabMarked(String input) {
+		String text = input;
+		text = text.replaceAll("<table>", "");
+		text = text.replaceAll("</table>", "");
+		text = text.replaceAll("<tr>", "");
+		text = text.replaceAll("</tr>", "");
+		text = text.replaceAll("<td>", "|    ");
+		text = text.replaceAll("</td>", "    |");
+		text = text.replaceAll("<br/>", "");
+		text = text.replaceAll("\n+", "\n");
+
+		if (text.startsWith("\n")) {
+			text = text.replaceFirst("\n", "");
+		}
+
+		return text;
+	}
+}
