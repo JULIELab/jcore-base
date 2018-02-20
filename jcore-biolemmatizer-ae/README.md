0f04e3b6dd3971414ee2443ee514a0088d93c506
Philipp Sieg <philippsieg@gmx.de>
2018-02-20 12:54:56 +0100

    Commit for saving during work in progress

---
+++ jcore-biolemmatizer-ae/README.md
@@ -0,0 +1,34 @@
+# BioLemmatizer
+
+**Descriptor Path**:
+```
+biolemmatizer.desc.jcore-biolemmatizer-ae
+```
+
+Lemmatization tool for morphological analysis of biomedical literature downloaded from SourceForge
+
+
+
+**1. Parameters**
+
+| Parameter Name | Parameter Type | Mandatory | Multivalued | Description |
+|----------------|----------------|-----------|-------------|-------------|
+| param1 | UIMA-Type | Boolean | Boolean | Description |
+| param2 | UIMA-Type | Boolean | Boolean | Description |
+
+**2. Predefined Settings**
+
+| Parameter Name | Parameter Syntax | Example |
+|----------------|------------------|---------|
+| param1 | Syntax-Description | `Example` |
+| param2 | Syntax-Description | `Example` |
+
+**3. Capabilities**
+
+| Type | Input | Output |
+|------|:-----:|:------:|
+| de.julielab.jcore.types.TYPE |  | `+` |
+| de.julielab.jcore.types.ace.TYPE | `+` |  |
+
+
+[1] Some Literature?
---
+++ jcore-biolemmatizer-ae/src/test/java/biolemmatizer/BioLemmatizerTest.java
@@ -0,0 +1,162 @@
+
+package biolemmatizer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.TypeSystem;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.junit.Test;
+
+import de.julielab.jcore.types.POSTag;
+import de.julielab.jcore.types.Token;
+/**
+ * Unit tests for jcore-biolemmatizer-ae.
+ * @author 
+ *
+ */
+public class BioLemmatizerTest{
+	
+	@Test
+	public void testBioLemmatizer() {
+		try {
+			JCas jCas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-all-types");
+			jCas.setDocumentText("Three horses were going contemplatively around bushy bushes.");
+			AnalysisEngine bioLemmatizer = AnalysisEngineFactory.createEngine(
+					"biolemmatizer.desc.jcore-biolemmatizer-ae");
+			TypeSystem typeSystem = jCas.getTypeSystem();
+			Feature posFeat = typeSystem.getFeatureByFullName("de.julielab.jcore.types.POSTag");
+			Token t;
+//			POSTag pos;
+			t = new Token(jCas, 0, 5);
+			t.setFeatureValueFromString(posFeat, "DT");
+//			pos = new POSTag(jCas, 0, 5);
+//			pos.setValue("DT");
+//			pos.addToIndexes();
+//			t.setPosTag(0, pos);
+			t.addToIndexes();
+			t = new Token(jCas, 6, 12);
+			t.setFeatureValueFromString(posFeat, "NNS");
+//			pos = new POSTag(jCas, 6, 12);
+//			pos.setValue("NNS");
+//			pos.addToIndexes();
+//			t.setPosTag(1, pos);
+			t.addToIndexes();
+			t = new Token(jCas, 13, 17);
+			t.setFeatureValueFromString(posFeat, "VBD");
+//			pos = new POSTag(jCas, 13, 17);
+//			pos.setValue("VBD");
+//			pos.addToIndexes();
+//			t.setPosTag(2, pos);
+			t.addToIndexes();
+			t = new Token(jCas, 18, 23);
+			t.setFeatureValueFromString(posFeat, "VBG");
+//			pos = new POSTag(jCas, 18, 23);
+//			pos.setValue("VBG");
+//			pos.addToIndexes();
+//			t.setPosTag(3, pos);
+			t.addToIndexes();
+			t = new Token(jCas, 24, 39);
+			t.setFeatureValueFromString(posFeat, "RB");
+//			pos = new POSTag(jCas, 24, 39);
+//			pos.setValue("RB");
+//			pos.addToIndexes();
+//			t.setPosTag(4, pos);
+			t.addToIndexes();
+			t = new Token(jCas, 40, 46);
+			t.setFeatureValueFromString(posFeat, "IN");
+//			pos = new POSTag(jCas, 40, 46);
+//			pos.setValue("IN");
+//			pos.addToIndexes();
+//			t.setPosTag(5, pos);
+			t.addToIndexes();
+			t = new Token(jCas, 47, 52);
+			t.setFeatureValueFromString(posFeat, "JJ");
+//			pos = new POSTag(jCas, 47, 52);
+//			pos.setValue("JJ");
+//			pos.addToIndexes();
+//			t.setPosTag(6, pos);
+			t.addToIndexes();
+			t = new Token(jCas, 53, 59);
+			t.setFeatureValueFromString(posFeat, "NNS");
+//			pos = new POSTag(jCas, 53, 59);
+//			pos.setValue("NNS");
+//			pos.addToIndexes();
+//			t.setPosTag(7, pos);
+			t.addToIndexes();
+			t = new Token(jCas, 59, 60);
+			t.setFeatureValueFromString(posFeat, ".");
+//			pos = new POSTag(jCas, 59, 60);
+//			pos.setValue(".");
+//			t.setPosTag(8, pos);
+			t.addToIndexes();
+//			PennBioIEPOSTag pos = new PennBioIEPOSTag(jCas);
+//			pos.setValue("DT");
+//			t.setPosTag(0, pos);
+//			t.setPosTag(0, "DT");
+//			t.setPosTag(1, "NNS");
+//			t.setPosTag(2, "VBD");
+//			t.setPosTag(3, "VBG");
+//			t.setPosTag(4, "RB");
+//			t.setPosTag(5, "IN");
+//			t.setPosTag(6, "JJ");
+//			t.setPosTag(7, "NNS");
+//			t.setPosTag(8, ".");
+			bioLemmatizer.process(jCas);
+			
+			FSIterator<Annotation> iterator = jCas.getAnnotationIndex(Token.type).iterator();
+			while (iterator.hasNext()) {
+				Token token = (Token) iterator.next();
+				assertNotNull(token.getLemma());
+				assertNotNull(token.getLemma().getValue());
+				assertEquals(token.getBegin(), token.getLemma().getBegin());
+				assertEquals(token.getEnd(), token.getLemma().getEnd());
+				String expectedLemma;
+				switch (token.getCoveredText()) {
+				case "Three":
+					expectedLemma = "Three";
+					break;
+				case "horses":
+					expectedLemma = "horse";
+					break;
+				case "were":
+					expectedLemma = "be";
+					break;
+				case "going":
+					expectedLemma = "go";
+					break;
+				case "contemplatively":
+					expectedLemma = "contemplative";
+					break;
+				case "around":
+					expectedLemma = "around";
+					break;
+				case "bushy":
+					expectedLemma = "bush";
+					break;
+				case "bushes":
+					expectedLemma = "bush";
+					break;
+				case ".":
+					expectedLemma = ".";
+					break;
+				default:
+					throw new IllegalStateException(
+							"Coding error in the test, all words should be covered or we could miss errors.");
+				}
+				assertEquals(token.getLemma().getValue(), expectedLemma);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
