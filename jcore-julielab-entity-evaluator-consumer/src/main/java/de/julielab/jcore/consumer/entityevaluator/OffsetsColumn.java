73a047e658dea12097553d1e61bcc1f4906618da
khituras <chew@gmx.net>
2018-02-20 22:26:10 +0100

    Switched from TreeMap to NavigatableMap where possible.

--- jcore-julielab-entity-evaluator-consumer/src/main/java/de/julielab/jcore/consumer/entityevaluator/OffsetsColumn.java
+++ jcore-julielab-entity-evaluator-consumer/src/main/java/de/julielab/jcore/consumer/entityevaluator/OffsetsColumn.java
@@ -2,7 +2,7 @@ package de.julielab.jcore.consumer.entityevaluator;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.NavigableMap;
 
 import org.apache.uima.jcas.cas.TOP;
 import org.apache.uima.jcas.tcas.Annotation;
@@ -16,10 +16,10 @@ public class OffsetsColumn extends Column {
 	private OffsetMode offsetMode;
 	private JCoReTreeMapAnnotationIndex<Long, ? extends Annotation> sentenceIndex;
 	private OffsetScope offsetScope;
-	private Map<Annotation, TreeMap<Integer, Integer>> numWsMaps;
-	private TreeMap<Integer, Integer> documentNumWsMap;
+	private Map<Annotation, NavigableMap<Integer, Integer>> numWsMaps;
+	private NavigableMap<Integer, Integer> documentNumWsMap;
 
-	public OffsetsColumn(TreeMap<Integer, Integer> numWsMap, OffsetMode offsetMode) {
+	public OffsetsColumn(NavigableMap<Integer, Integer> numWsMap, OffsetMode offsetMode) {
 		this.documentNumWsMap = numWsMap;
 		this.offsetMode = offsetMode;
 		this.offsetScope = OffsetScope.Document;
@@ -42,7 +42,7 @@ public class OffsetsColumn extends Column {
 	@Override
 	public String getValue(TOP a) {
 		Annotation an = (Annotation) a;
-		TreeMap<Integer, Integer> numWsMap = documentNumWsMap;
+		NavigableMap<Integer, Integer> numWsMap = documentNumWsMap;
 		int annotationOffset = 0;
 
 		if (offsetScope == OffsetScope.Sentence) {
@@ -63,8 +63,8 @@ public class OffsetsColumn extends Column {
 	 * @return A map that can be queried for each position of the sentence text
 	 *         how many whitespaces before that position exist.
 	 */
-	private TreeMap<Integer, Integer> getNumWsMapForSentence(Annotation s) {
-		TreeMap<Integer, Integer> numWsMap = numWsMaps.get(s);
+	private NavigableMap<Integer, Integer> getNumWsMapForSentence(Annotation s) {
+		NavigableMap<Integer, Integer> numWsMap = numWsMaps.get(s);
 		if (numWsMap == null) {
 			numWsMap = EntityEvaluatorConsumer.createNumWsMap(s.getCoveredText());
 			numWsMaps.put(s, numWsMap);
@@ -72,7 +72,7 @@ public class OffsetsColumn extends Column {
 		return numWsMap;
 	}
 
-	private String getOffsets(Annotation an, TreeMap<Integer, Integer> numWsMap, int annotationOffset) {
+	private String getOffsets(Annotation an, NavigableMap<Integer, Integer> numWsMap, int annotationOffset) {
 		int beginOffset;
 		int endOffset;
 		switch (offsetMode) {
