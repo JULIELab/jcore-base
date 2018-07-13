c2050c1f25b43a6e19264624c39c0e8978f79968
khituras <chew@gmx.net>
2018-07-13 09:25:29 +0200

    Handled a rare error with acronym resolution but did no solve it.
    When the input text containes newlines it might happen that an
    abbreviation long form has the newline in it. Actually, this was only
    observed with faulty documents (PMID 26306035 has some weird abstract
    section causing the issue) or sentence splitting errors. We now catch
    the issue, do not create a new Mention and continue.

--- jcore-banner-ae/src/main/java/banner/postprocessing/LocalAbbreviationPostProcessor.java
+++ jcore-banner-ae/src/main/java/banner/postprocessing/LocalAbbreviationPostProcessor.java
@@ -9,71 +9,63 @@ import banner.types.Sentence;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LocalAbbreviationPostProcessor implements PostProcessor
-{
-	private final static Logger log = LoggerFactory.getLogger(LocalAbbreviationPostProcessor.class);
-	private ExtractAbbrev extractAbbrev;
+public class LocalAbbreviationPostProcessor implements PostProcessor {
+    private final static Logger log = LoggerFactory.getLogger(LocalAbbreviationPostProcessor.class);
+    private ExtractAbbrev extractAbbrev;
 
-	public LocalAbbreviationPostProcessor()
-	{
-		extractAbbrev = new ExtractAbbrev();
-	}
+    public LocalAbbreviationPostProcessor() {
+        extractAbbrev = new ExtractAbbrev();
+    }
 
-	private void processAbbreviation(Mention formFound, String formNotFound)
-	{
-		Sentence sentence = formFound.getSentence();
-		EntityType type = formFound.getEntityType();
-		int charIndex = sentence.getText().indexOf(formNotFound);
-		int start = sentence.getTokenIndex(charIndex, true);
-		int end = sentence.getTokenIndex(charIndex + formNotFound.length(), false);
-		if (start == end)
-			return;
-			Mention newMention;
-		try {
-			newMention = new Mention(sentence, start, end, type, formFound.getMentionType(), formFound.getProbability());
-		} catch (Exception e) {
-		    log.error("Exception occurred while creating a new entity mention for an abbrevation pair." +
-                    " The string to be tagged as another mention is \"{}\", its character offset in the text" +
-                    " sentence was determined as {}, its start token index as {} and its end token index as {}", formNotFound,
+    private void processAbbreviation(Mention formFound, String formNotFound) {
+        Sentence sentence = formFound.getSentence();
+        EntityType type = formFound.getEntityType();
+        int charIndex = sentence.getText().indexOf(formNotFound);
+        int start = sentence.getTokenIndex(charIndex, true);
+        int end = sentence.getTokenIndex(charIndex + formNotFound.length(), false);
+        if (start == end)
+            return;
+        Mention newMention = null;
+        try {
+            newMention = new Mention(sentence, start, end, type, formFound.getMentionType(), formFound.getProbability());
+        } catch (Exception e) {
+            log.warn("Exception occurred while creating a new entity mention for an abbrevation pair." +
+                            " The string to be tagged as another mention is \"{}\", its character offset in the text" +
+                            " sentence was determined as {}, its start token index as {} and its end token index as {}." +
+                            "No new mention will be added and processing will continue.", formNotFound,
                     charIndex,
                     start, end);
-			throw e;
-		}
-		boolean overlaps = false;
-		for (Mention mention : sentence.getMentions())
-			overlaps |= mention.overlaps(newMention);
-		if (!overlaps)
-			sentence.addMention(newMention);
-	}
+        }
+        if (newMention != null) {
+            boolean overlaps = false;
+            for (Mention mention : sentence.getMentions())
+                overlaps |= mention.overlaps(newMention);
+            if (!overlaps)
+                sentence.addMention(newMention);
+        }
+    }
 
-	public void postProcess(Sentence sentence)
-	{
-		Set<AbbreviationPair> abbreviationPairs = extractAbbrev.extractAbbrPairs(sentence.getText());
-		if (abbreviationPairs.size() > 0)
-		{
-			for (AbbreviationPair abbreviation : abbreviationPairs)
-			{
-				Mention shortMention = null;
-				Mention longMention = null;
-				for (Mention mention : sentence.getMentions())
-				{
-					if (abbreviation.getShortForm().equals(mention.getText()))
-						shortMention = mention;
-					if (abbreviation.getLongForm().equals(mention.getText()))
-						longMention = mention;
-				}
-				if (shortMention == null)
-				{
-					if (longMention != null)
-						processAbbreviation(longMention, abbreviation.getShortForm());
-				}
-				else
-				{
-					if (longMention == null)
-						processAbbreviation(shortMention, abbreviation.getLongForm());
-				}
-			}
-		}
-	}
+    public void postProcess(Sentence sentence) {
+        Set<AbbreviationPair> abbreviationPairs = extractAbbrev.extractAbbrPairs(sentence.getText());
+        if (abbreviationPairs.size() > 0) {
+            for (AbbreviationPair abbreviation : abbreviationPairs) {
+                Mention shortMention = null;
+                Mention longMention = null;
+                for (Mention mention : sentence.getMentions()) {
+                    if (abbreviation.getShortForm().equals(mention.getText()))
+                        shortMention = mention;
+                    if (abbreviation.getLongForm().equals(mention.getText()))
+                        longMention = mention;
+                }
+                if (shortMention == null) {
+                    if (longMention != null)
+                        processAbbreviation(longMention, abbreviation.getShortForm());
+                } else {
+                    if (longMention == null)
+                        processAbbreviation(shortMention, abbreviation.getLongForm());
+                }
+            }
+        }
+    }
 
 }
