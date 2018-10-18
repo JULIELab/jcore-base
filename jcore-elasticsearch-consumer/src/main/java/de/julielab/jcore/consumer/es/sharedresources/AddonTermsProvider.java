0d4fd4a1513a37dd9af78cd5d5ddeadcb9bc36e7
khituras <chew@gmx.net>
2018-10-18 09:06:52 +0200

    The addon terms provider does not accept JSON arrays as the second column.

--- jcore-elasticsearch-consumer/src/main/java/de/julielab/jcore/consumer/es/sharedresources/AddonTermsProvider.java
+++ jcore-elasticsearch-consumer/src/main/java/de/julielab/jcore/consumer/es/sharedresources/AddonTermsProvider.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import de.julielab.jcore.utility.JCoReTools;
 import org.apache.commons.io.IOUtils;
 import org.apache.uima.resource.DataResource;
@@ -14,51 +16,58 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AddonTermsProvider implements IAddonTermsProvider {
-	Logger log = LoggerFactory.getLogger(AddonTermsProvider.class);
+    Logger log = LoggerFactory.getLogger(AddonTermsProvider.class);
 
-	private Map<String, String[]> addonTerms;
-	
-	@Override
-	public void load(DataResource aData) throws ResourceInitializationException {
-		try {
-			addonTerms = new HashMap<>();
-			log.info("Loading addon terms from " + aData.getUri());
-			int addons = 0;
-			InputStream inputStream;
-			try {
-				inputStream = JCoReTools.resolveExternalResourceGzipInputStream(aData);
-			} catch (Exception e) {
-				throw new IOException("Could not read from " + aData.getUri() + ": Resource not found.");
-			}
-			List<String> addonLines = IOUtils.readLines(inputStream, "UTF-8");
-			for (String line : addonLines) {
-				if (line.trim().length() == 0 || line.startsWith("#"))
-					continue;
-				String[] mapping = line.split("\t");
-				if (mapping.length != 2)
-					throw new IllegalArgumentException("Format problem with addon terms line " + line + ": Not exactly one tab character.");
-				// we use internalization to reduce memory
-				// requirements
-				String term = mapping[0].trim().intern();
-				String[] addonArray = mapping[1].split("\\|");
-				for (int i = 0; i < addonArray.length; i++) {
-					String trimmedAddon = addonArray[i].trim();
-					// we use internalization to reduce memory
-					// requirements
-					addonArray[i] = trimmedAddon.intern();
-					addons++;
-				}
-				addonTerms.put(term, addonArray);
-			}
-			log.info("Loaded {} addons for {} terms.", addons, addonTerms.size());
-		} catch (IOException e) {
-			throw new ResourceInitializationException(e);
-		}
-	}
+    private Map<String, String[]> addonTerms;
 
-	@Override
-	public Map<String, String[]> getAddonTerms() {
-		return addonTerms;
-	}
+    @Override
+    public void load(DataResource aData) throws ResourceInitializationException {
+        try {
+            addonTerms = new HashMap<>();
+            log.info("Loading addon terms from " + aData.getUri());
+            int addons = 0;
+            InputStream inputStream;
+            try {
+                inputStream = JCoReTools.resolveExternalResourceGzipInputStream(aData);
+            } catch (Exception e) {
+                throw new IOException("Could not read from " + aData.getUri() + ": Resource not found.");
+            }
+            List<String> addonLines = IOUtils.readLines(inputStream, "UTF-8");
+            for (String line : addonLines) {
+                if (line.trim().length() == 0 || line.startsWith("#"))
+                    continue;
+                String[] mapping = line.split("\t");
+                if (mapping.length != 2)
+                    throw new IllegalArgumentException("Format problem with addon terms line " + line + ": Not exactly one tab character.");
+                // we use internalization to reduce memory
+                // requirements
+                String term = mapping[0].trim().intern();
+                String[] addonArray;
+                if (mapping[1].startsWith("[\"") && mapping[1].endsWith("\"]")) {
+                    // This looks like a JSON array
+                    Gson gson = new Gson();
+                    addonArray = gson.fromJson(mapping[1], String[].class);
+                } else {
+                    addonArray = mapping[1].split("\\|");
+                }
+                for (int i = 0; i < addonArray.length; i++) {
+                    String trimmedAddon = addonArray[i].trim();
+                    // we use internalization to reduce memory
+                    // requirements
+                    addonArray[i] = trimmedAddon.intern();
+                    addons++;
+                }
+                addonTerms.put(term, addonArray);
+            }
+            log.info("Loaded {} addons for {} terms.", addons, addonTerms.size());
+        } catch (IOException e) {
+            throw new ResourceInitializationException(e);
+        }
+    }
+
+    @Override
+    public Map<String, String[]> getAddonTerms() {
+        return addonTerms;
+    }
 
 }
