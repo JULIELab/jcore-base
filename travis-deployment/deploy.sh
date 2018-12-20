2b0d64292be659ca2250e60b2329507fe4db3c2c
khituras <chew@gmx.net>
2018-12-20 11:28:34 +0100

    Working on the script for selective artifact deployment

--- travis-deployment/deploy.sh
+++ travis-deployment/deploy.sh
@@ -1,18 +1,35 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	echo "Executing deploy"
-	wget https://search.maven.org/remotecontent?filepath=de/julielab/julie-xml-tools/0.4.2/julie-xml-tools-0.4.2-xml-tools-assembly.jar --output-document julie-xml-tools.jar
+	if [ ! -f julie-xml-tools.jar ]; then
+	    wget https://search.maven.org/remotecontent?filepath=de/julielab/julie-xml-tools/0.4.2/julie-xml-tools-0.4.2-xml-tools-assembly.jar --output-document julie-xml-tools.jar
+	fi
+	if [ ! -f julie-xml-tools.jar ]; then
+    	    # TODO: Download the aether tools
+    fi
 
 
 	for i in `java -jar julie-xml-tools.jar pom.xml //module`; do
-	    java -cp julielab-maven-aether-utilities.jar de.julielab.utilities.aether.apps.GetCoordinatesFromPom $i/pom.xml > coords.txt;
+	    java -cp julielab-maven-aether-utilities.jar de.julielab.utilities.aether.apps.GetCoordinatesFromRawPom $i/pom.xml > coords.txt;
 	    groupId=`grep 'GROUPID:' coords.txt | sed 's/^GROUPID: //'`
 	    artifactId=`grep 'ARTIFACTID:' coords.txt | sed 's/^ARTIFACTID: //'`
 	    version=`grep 'VERSION:' coords.txt | sed 's/^VERSION: //'`
-	    if [ `java -cp julielab-maven-aether-utilities.jar de.julielab.utilities.aether.apps.FindRemoteChecksum` ]; then
-
+	    packaging=`grep 'PACKAGING:' coords.txt | sed 's/^PACKAGING: //'`
+	    artifactFile=$i/target/$artifactId-$version.$packaging
+	    if [ ! -f $artifactFile ]; then
+	        echo "Could not find the expected artifact file $artifactFile. Has the project successfully been built?"
+	    else
+	        checksum=`md5 -r $artifactFile | grep -io '^[0-9a-z]*'`
+	        echo "Trying to find MD5 checksum $checksum of artifact $groupId:$artifactId:$packaging:$version"
+	        csFound=`java -cp julielab-maven-aether-utilities.jar de.julielab.utilities.aether.apps.FindRemoteChecksum $groupId:$artifactId:$packaging:$version $checksum`
+            if [ "$csFound" == "true" ]; then
+                echo "Found the checksum, not deploying again."
+            else
+                echo "Checksum was not found, deploying the artifact."
+                mvn deploy -P sonatype-nexus-deployment --settings travis-deployment/mvnsettings.xml -DskipTests=true
+            fi
+	    fi
     done
-#    mvn deploy -P sonatype-nexus-deployment --settings travis-deployment/mvnsettings.xml -DskipTests=true
 else
 	echo "Deploy not executed"
 fi
