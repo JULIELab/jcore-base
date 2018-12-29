da0ca6a5c39eff76161e01c84b3f3463b1618765
khituras <chew@gmx.net>
2018-12-29 10:02:02 +0100

    Adding fixes to actually commit the jedis components

--- travis-deployment/deploy.sh
+++ travis-deployment/deploy.sh
@@ -20,22 +20,24 @@ if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
         csNotFound=`java -cp julielab-maven-aether-utilities.jar de.julielab.utilities.aether.apps.GetRemoteChecksums $groupId:$artifactId:$packaging:$version | grep '<checkums not found>'`
 	    if [[ $version =~ .*SNAPSHOT.* ]] || [ "$csNotFound" == "<checkums not found>" ]; then
             echo "This is a SNAPSHOT or a release that has not yet been deployed. Deploying."
-            mvn deploy -f $i/pom.xml -P sonatype-nexus-deployment --settings travis-deployment/mvnsettings.xml -DskipTests=true -N
+            mvn deploy -B -f $i/pom.xml -P sonatype-nexus-deployment --settings travis-deployment/mvnsettings.xml -DskipTests=true -N
 	    fi
     done
 
+    echo "Now deploying JeDIS components"
     for i in . `java -jar julie-xml-tools.jar jedis-parent/pom.xml //module`; do
-    	    java -cp julielab-maven-aether-utilities.jar de.julielab.utilities.aether.apps.GetCoordinatesFromRawPom $i/pom.xml > coords.txt;
+            path=`echo $i | sed 's|../||'
+    	    java -cp julielab-maven-aether-utilities.jar de.julielab.utilities.aether.apps.GetCoordinatesFromRawPom $path/pom.xml > coords.txt;
     	    groupId=`grep 'GROUPID:' coords.txt | sed 's/^GROUPID: //'`
     	    artifactId=`grep 'ARTIFACTID:' coords.txt | sed 's/^ARTIFACTID: //'`
     	    version=`grep 'VERSION:' coords.txt | sed 's/^VERSION: //'`
     	    packaging=`grep 'PACKAGING:' coords.txt | sed 's/^PACKAGING: //'`
-    	    artifactFile=$i/target/$artifactId-$version.$packaging
+    	    artifactFile=$path/target/$artifactId-$version.$packaging
             echo "Checking if $groupId:$artifactId:$packaging:$version exists"
             csNotFound=`java -cp julielab-maven-aether-utilities.jar de.julielab.utilities.aether.apps.GetRemoteChecksums $groupId:$artifactId:$packaging:$version | grep '<checkums not found>'`
     	    if [[ $version =~ .*SNAPSHOT.* ]] || [ "$csNotFound" == "<checkums not found>" ]; then
                 echo "This is a SNAPSHOT or a release that has not yet been deployed. Deploying."
-                mvn deploy -f $i/pom.xml -P sonatype-nexus-deployment --settings travis-deployment/mvnsettings.xml -DskipTests=true -N
+                mvn deploy -B -f $path/pom.xml -P sonatype-nexus-deployment --settings travis-deployment/mvnsettings.xml -DskipTests=true -N
     	    fi
         done
 else
