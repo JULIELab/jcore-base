04da990ea12b81022de1e4627f4eb813d8a4c634
khituras <chew@gmx.net>
2019-01-12 16:34:01 +0100

    Commenting out all references to the stanford pos tagger because I could not find any mavenized version or the correct source code of a version that would work with lingscope.

--- jcore-lingscope-ae/src/main/java/lingscope/algorithms/PosTagger.java
+++ jcore-lingscope-ae/src/main/java/lingscope/algorithms/PosTagger.java
@@ -1,6 +1,8 @@
 package lingscope.algorithms;
 
-import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+//import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -12,18 +14,19 @@ import java.util.logging.Logger;
  */
 public class PosTagger {
 
-    private MaxentTagger posTagger;
+    //private MaxentTagger posTagger;
 
     /**
      * Creates an instance of POS tagger by loading the given grammar file
      * @param grammarFile
      */
     public PosTagger(String grammarFile) {
-        try {
-            posTagger = new MaxentTagger(grammarFile);
-        } catch (Exception ex) {
-            Logger.getLogger(PosTagger.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        throw new IllegalArgumentException("This class is not implemented because it required a specific version of the stanford-postagger that could neither be found on the stanford postagger web page nor in stanford core nlp.");
+//        try {
+//            posTagger = new MaxentTagger(grammarFile);
+//        } catch (Exception ex) {
+//            Logger.getLogger(PosTagger.class.getName()).log(Level.SEVERE, null, ex);
+//        }
     }
 
     /**
@@ -34,21 +37,22 @@ public class PosTagger {
      * @return
      */
     public List<String> replaceWordsWithPos(String sentence, boolean isTokenized) {
-        if (!isTokenized) {
-            sentence = AbnerTokenizer.splitTermsByPunctuation(sentence);
-        }
-        List<String> ret = new ArrayList<String>();
-        String tagged = "";
-        try {
-            tagged = posTagger.tagString(sentence);
-        } catch (Exception ex) {
-            Logger.getLogger(PosTagger.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        for (String wordTag : tagged.split(" +")) {
-            String[] tags = wordTag.split("/");
-            String tag = tags[tags.length - 1];
-            ret.add(tag);
-        }
-        return ret;
+        throw new IllegalArgumentException("This class is not implemented because it required a specific version of the stanford-postagger that could neither be found on the stanford postagger web page nor in stanford core nlp.");
+//        if (!isTokenized) {
+//            sentence = AbnerTokenizer.splitTermsByPunctuation(sentence);
+//        }
+//        List<String> ret = new ArrayList<String>();
+//        String tagged = "";
+//        try {
+//            tagged = posTagger.tagString(sentence);
+//        } catch (Exception ex) {
+//            Logger.getLogger(PosTagger.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        for (String wordTag : tagged.split(" +")) {
+//            String[] tags = wordTag.split("/");
+//            String tag = tags[tags.length - 1];
+//            ret.add(tag);
+//        }
+//        return ret;
     }
 }
