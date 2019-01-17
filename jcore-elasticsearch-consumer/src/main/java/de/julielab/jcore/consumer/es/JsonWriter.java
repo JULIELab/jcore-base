a838fc37e2d897dbca8d07de16c78416af38f952
khituras <chew@gmx.net>
2019-01-17 15:14:33 +0100

    Restructuring the JsonWriter to just create one file per thread.
    The file channel solution seemed to brittle after all because
    we work a lot over the network where file locking does not work as
    intended.

--- jcore-elasticsearch-consumer/src/main/java/de/julielab/jcore/consumer/es/JsonWriter.java
+++ jcore-elasticsearch-consumer/src/main/java/de/julielab/jcore/consumer/es/JsonWriter.java
@@ -1,9 +1,13 @@
 package de.julielab.jcore.consumer.es;
 
 import java.io.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -29,20 +33,17 @@ public class JsonWriter extends AbstractCasToJsonConsumer {
     public static final String PARAM_FILE_OUTPUT = "FileOutput";
     public static final String PARAM_MAX_FILE_SIZE = "MaxFileSize";
     private static final Logger log = LoggerFactory.getLogger(AbstractCasToJsonConsumer.class);
+    //@ConfigurationParameter(name = PARAM_MAX_FILE_SIZE, mandatory = false, description = "Only in effect when " + PARAM_FILE_OUTPUT + "is set to 'true'. Then, when the resulting file surpasses the given size, the current file is closed and a new one is opened. The files receive a number suffix. This can be helpful for large document collections to be written into GZIP format because in file mode only the complete file can be GZIPPed. Defaults to 0 which deactivates this function.", defaultValue = "0")
+    //private Long maxFileSize;
+    private static Integer writerNumber;
     private List<Document> documentBatch = new ArrayList<>();
-    @ConfigurationParameter(name = PARAM_OUTPUT_DEST, description = "The path to which the JSON data will be stored. This parameter can denote a file or a directory. See the " + PARAM_FILE_OUTPUT + " parameter which specifies whether the output should be written into one large file - one document per line - or into a directory - one document per file. The file or directory will be created if it does not exist, including all parent directories. In the directory case, the files in the directory will be overwritten. In the single file case, new documents will be appended, requiring the file to be deleted manually if desired.")
+    @ConfigurationParameter(name = PARAM_OUTPUT_DEST, description = "The path to which the JSON data will be stored. This parameter can denote a file name (without extension) or a directory. See the " + PARAM_FILE_OUTPUT + " parameter which specifies whether the output should be written into large files - one document per line, one file per thread - or into a directory - one document per file. The files or directory will be created if they does not exist, including all parent directories. All files will be overwritten.")
     private File outputDest;
     @ConfigurationParameter(name = PARAM_GZIP, mandatory = false)
     private Boolean gzip;
     @ConfigurationParameter(name = PARAM_FILE_OUTPUT, description = "This boolean parameter determines whether a single file (parameter set to 'true') or a directory of files (parameter set to 'false') will be output to the location given with " + PARAM_OUTPUT_DEST + ". File output supports multithreading on the same machine through  ")
     private Boolean fileMode;
-    //@ConfigurationParameter(name = PARAM_MAX_FILE_SIZE, mandatory = false, description = "Only in effect when " + PARAM_FILE_OUTPUT + "is set to 'true'. Then, when the resulting file surpasses the given size, the current file is closed and a new one is opened. The files receive a number suffix. This can be helpful for large document collections to be written into GZIP format because in file mode only the complete file can be GZIPPed. Defaults to 0 which deactivates this function.", defaultValue = "0")
-    //private Long maxFileSize;
-
-    private RandomAccessFile randomAccessFile;
-    private FileChannel channel;
-    private static Object lock = new Object();
-
+    private BufferedWriter bw;
 
     @Override
     public void initialize(UimaContext aContext) throws ResourceInitializationException {
@@ -50,10 +51,11 @@ public class JsonWriter extends AbstractCasToJsonConsumer {
         outputDest = new File((String) aContext.getConfigParameterValue(PARAM_OUTPUT_DEST));
         gzip = Optional.ofNullable((Boolean) aContext.getConfigParameterValue(PARAM_GZIP)).orElse(false);
         fileMode = (Boolean) aContext.getConfigParameterValue(PARAM_FILE_OUTPUT);
-     //   maxFileSize = Optional.ofNullable((Long) aContext.getConfigParameterValue(PARAM_MAX_FILE_SIZE)).orElse(0L);
+        //   maxFileSize = Optional.ofNullable((Long) aContext.getConfigParameterValue(PARAM_MAX_FILE_SIZE)).orElse(0L);
+
         if (!outputDest.exists()) {
             if (!fileMode)
-            outputDest.mkdirs();
+                outputDest.mkdirs();
             else {
                 final File parentFile = outputDest.getParentFile();
                 if (parentFile != null && !parentFile.exists())
@@ -61,9 +63,11 @@ public class JsonWriter extends AbstractCasToJsonConsumer {
             }
         }
         if (fileMode) {
+            synchronized (JsonWriter.class) {
+                outputDest = new File(outputDest.getAbsolutePath() + File.separator + getHostName() + "-" + ++writerNumber + ".json");
+            }
             try {
-                randomAccessFile = new RandomAccessFile(outputDest, "rwd");
-                channel = randomAccessFile.getChannel();
+                bw = Files.newBufferedWriter(outputDest.toPath(), StandardOpenOption.WRITE);
             } catch (FileNotFoundException e) {
                 log.error("Could not access output destination {} for writing", outputDest, e);
                 throw new ResourceInitializationException(e);
@@ -75,6 +79,18 @@ public class JsonWriter extends AbstractCasToJsonConsumer {
         log.info("{}: {}", PARAM_GZIP, gzip);
         log.info("{}: {}", PARAM_FILE_OUTPUT, fileMode);
         //log.info("{}: {}", PARAM_MAX_FILE_SIZE, maxFileSize);
+    }
+
+    private String getHostName() {
+        InetAddress address;
+        String hostName;
+        try {
+            address = InetAddress.getLocalHost();
+            hostName = address.getHostName();
+        } catch (UnknownHostException e) {
+            throw new IllegalStateException(e);
+        }
+        return hostName;
     }
 
     @Override
@@ -111,25 +127,16 @@ public class JsonWriter extends AbstractCasToJsonConsumer {
                 throw new AnalysisEngineProcessException(e);
             }
         } else {
-            synchronized (lock) {
-                // Add the documents to the single file, one per line
-                ByteBuffer newlineBuf = ByteBuffer.wrap(System.getProperty("line.separator").getBytes());
-                try {
-                    for (Document document : documentBatch) {
-                        String json = gson.toJson(document);
-                        ByteBuffer buffer = ByteBuffer.wrap(json.getBytes());
-                        try (FileLock ignored = channel.lock()) {
-                            randomAccessFile.seek(outputDest.length());
-                            while (buffer.hasRemaining())
-                                channel.write(buffer);
-                            newlineBuf.rewind();
-                            while(newlineBuf.hasRemaining())
-                                channel.write(newlineBuf);
-                        }
-                    }
-                } catch (IOException e) {
-                    throw new AnalysisEngineProcessException(e);
+            // Add the documents to the single file, one per line
+            ByteBuffer newlineBuf = ByteBuffer.wrap(System.getProperty("line.separator").getBytes());
+            try {
+                for (Document document : documentBatch) {
+                    String json = gson.toJson(document);
+                    bw.write(json);
+                    bw.newLine();
                 }
+            } catch (IOException e) {
+                throw new AnalysisEngineProcessException(e);
             }
         }
         documentBatch.clear();
@@ -144,11 +151,11 @@ public class JsonWriter extends AbstractCasToJsonConsumer {
     @Override
     public void collectionProcessComplete() throws AnalysisEngineProcessException {
         writeDocumentBatch();
-        if (channel != null) {
+        if (bw != null) {
             try {
-                channel.close();
+                bw.close();
             } catch (IOException e) {
-                log.error("Could not close channel to the current output file", e);
+                log.error("Could not close writer to the current output file", e);
                 throw new AnalysisEngineProcessException(e);
             }
         }
