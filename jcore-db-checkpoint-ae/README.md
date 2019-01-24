aa64f426134a0cd5f55c6cf007eb03459dcfb34d
khituras <chew@gmx.net>
2019-01-24 22:57:00 +0100

    Adding the jcore-db-checkpoint-ae. This component is the last missing piece of the JeDIS system. Currently, no component exists that marks documents in a subset table as being processed. Only 'is in process' is used by the database reader. This component can set the 'is processed' flag and set a configurable 'last component' value.

---
+++ jcore-db-checkpoint-ae/README.md
@@ -0,0 +1,34 @@
+# JCoRe Database Checkpoint AE
+
+**Descriptor Path**:
+```
+de.julielab.desc.jcore-db-checkpoint-ae
+```
+
+This is a JEDiS component. It can be used to set the 'last component' column in a subset table. This help to keep track of the pipeline status.
+
+
+
+**1. Parameters**
+
+| Parameter Name | Parameter Type | Mandatory | Multivalued | Description |
+|----------------|----------------|-----------|-------------|-------------|
+| param1 | UIMA-Type | Boolean | Boolean | Description |
+| param2 | UIMA-Type | Boolean | Boolean | Description |
+
+**2. Predefined Settings**
+
+| Parameter Name | Parameter Syntax | Example |
+|----------------|------------------|---------|
+| param1 | Syntax-Description | `Example` |
+| param2 | Syntax-Description | `Example` |
+
+**3. Capabilities**
+
+| Type | Input | Output |
+|------|:-----:|:------:|
+| de.julielab.jcore.types.TYPE |  | `+` |
+| de.julielab.jcore.types.ace.TYPE | `+` |  |
+
+
+[1] Some Literature?
---
+++ jcore-db-checkpoint-ae/src/main/java/de/julielab/jcore/ae/checkpoint/DBCheckpointAE.java
@@ -0,0 +1,177 @@
+package de.julielab.jcore.ae.checkpoint;
+
+import de.julielab.jcore.types.ext.DBProcessingMetaData;
+import de.julielab.jcore.utility.JCoReTools;
+import de.julielab.xmlData.Constants;
+import de.julielab.xmlData.config.FieldConfig;
+import de.julielab.xmlData.dataBase.CoStoSysConnection;
+import de.julielab.xmlData.dataBase.DataBaseConnector;
+import org.apache.commons.lang.StringUtils;
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.descriptor.ResourceMetaData;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.sql.BatchUpdateException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@ResourceMetaData(name = "JCoRe Database Checkpoint AE", description = "This component can be used when using a JCoRe database reader that reads from a CoStoSys/JeDIS subset. Enters the configured component name in the 'last component' column. Can also mark documents as being completely processed.")
+public class DBCheckpointAE extends JCasAnnotator_ImplBase {
+
+    public static final String PARAM_CHECKPOINT_NAME = "CheckpointName";
+    public static final String PARAM_INDICATE_FINISHED = "IndicateFinished";
+    public static final String PARAM_TABLE_SCHEMA = "TableSchema";
+    public static final String PARAM_COSTOSYS_CONFIG = "CostosysConfigFile";
+    public static final String PARAM_WRITE_BATCH_SIZE = "WriteBatchSize";
+    private final static Logger log = LoggerFactory.getLogger(DBCheckpointAE.class);
+    private DataBaseConnector dbc;
+
+    @ConfigurationParameter(name = PARAM_TABLE_SCHEMA)
+    private String schemaDocument;
+
+    @ConfigurationParameter(name = PARAM_CHECKPOINT_NAME)
+    private String componentDbName;
+
+    @ConfigurationParameter(name = PARAM_INDICATE_FINISHED, mandatory = false)
+    private boolean indicateFinished;
+
+    @ConfigurationParameter(name = PARAM_COSTOSYS_CONFIG, description = "File path or classpath resource location " +
+            "of a Corpus Storage System (CoStoSys) configuration file. This file specifies the database to " +
+            "write the XMI data into and the data table schema. This schema must at least define the primary key " +
+            "columns that the storage tables should have for each document. The primary key is currently just " +
+            "the document ID. Thus, at the moment, primary keys can only consist of a single element when using " +
+            "this component. This is a shortcoming of this specific component and must be changed here, if necessary.")
+    private String dbcConfigPath;
+
+    @ConfigurationParameter(name = PARAM_WRITE_BATCH_SIZE, mandatory = false, defaultValue = "50", description =
+            "The number of processed CASes after which the checkpoint should be written into the database. Defaults to 50.")
+    private int writeBatchSize;
+
+    private String subsetTable;
+
+    private List<DocumentId> docIds;
+
+    /**
+     * This method is called a single time by the framework at component
+     * creation. Here, descriptor parameters are read and initial setup is done.
+     */
+    @Override
+    public void initialize(final UimaContext aContext) throws ResourceInitializationException {
+        schemaDocument = (String) aContext.getConfigParameterValue(PARAM_TABLE_SCHEMA);
+        componentDbName = (String) aContext.getConfigParameterValue(PARAM_CHECKPOINT_NAME);
+        dbcConfigPath = (String) aContext.getConfigParameterValue(PARAM_COSTOSYS_CONFIG);
+        indicateFinished = Optional.ofNullable((Boolean) aContext.getConfigParameterValue(PARAM_INDICATE_FINISHED)).orElse(false);
+        writeBatchSize = Optional.ofNullable((Integer) aContext.getConfigParameterValue(PARAM_WRITE_BATCH_SIZE)).orElse(50);
+        try {
+            dbc = new DataBaseConnector(dbcConfigPath);
+        } catch (FileNotFoundException e) {
+            log.error("Could not initiate database connector", e);
+            throw new ResourceInitializationException(e);
+        }
+        docIds = new ArrayList<>();
+    }
+
+    @Override
+    public void batchProcessComplete() throws AnalysisEngineProcessException {
+        super.batchProcessComplete();
+        try (CoStoSysConnection conn = dbc.obtainOrReserveConnection()) {
+            setLastComponent(conn, subsetTable, docIds);
+        }
+    }
+
+    @Override
+    public void collectionProcessComplete() throws AnalysisEngineProcessException {
+        super.collectionProcessComplete();
+        try (CoStoSysConnection conn = dbc.obtainOrReserveConnection()) {
+            setLastComponent(conn, subsetTable, docIds);
+        }
+    }
+
+    /**
+     * This method is called for each document going through the component. This
+     * is where the actual work happens.
+     */
+    @Override
+    public void process(final JCas aJCas) throws AnalysisEngineProcessException {
+        DocumentId documentId;
+        String docId;
+        try {
+            final DBProcessingMetaData dbProcessingMetaData = JCasUtil.selectSingle(aJCas, DBProcessingMetaData.class);
+            documentId = new DocumentId(dbProcessingMetaData);
+            if (subsetTable == null)
+                subsetTable = dbProcessingMetaData.getSubsetTable();
+            docIds.add(documentId);
+        } catch (IllegalArgumentException e) {
+            docId = JCoReTools.getDocId(aJCas);
+            log.error("The document with document ID {} does not have an annotation of type {}. This annotation ought to contain the name of the subset table. It should be set by the DB reader. Cannot write the checkpoint to the datbase since the target subset table is unknown..", docId, DBProcessingMetaData.class.getCanonicalName());
+            throw new AnalysisEngineProcessException(e);
+        }
+    }
+
+    /**
+     * Writes the component name to the database subset
+     *
+     * @param conn
+     * @throws AnalysisEngineProcessException
+     */
+    private void setLastComponent(CoStoSysConnection conn, String subsetTableName, List<DocumentId> processedDocumentIds) throws AnalysisEngineProcessException {
+        if (processedDocumentIds.isEmpty() || StringUtils.isBlank(subsetTableName))
+            return;
+
+        FieldConfig annotationFieldConfig = dbc.getFieldConfiguration(schemaDocument);
+        String[] primaryKey = annotationFieldConfig.getPrimaryKey();
+        if (primaryKey.length > 1)
+            throw new IllegalArgumentException("Currently, only one-element primary keys are supported.");
+        // create a string for the prepared statement in the form "pk1 = ? AND pk2 = ? ..."
+        String primaryKeyPsString = StringUtils.join(annotationFieldConfig.expandPKNames("%s = ?"), " AND ");
+
+        log.debug("Marking {} documents to having been processed by component \"{}\".", processedDocumentIds.size(), componentDbName);
+
+        String sql = String.format("UPDATE %s SET %s='%s' WHERE %s", subsetTableName, Constants.LAST_COMPONENT, componentDbName, primaryKeyPsString);
+
+        try {
+            boolean tryagain;
+            do {
+                tryagain = false;
+                PreparedStatement ps = conn.prepareStatement(sql);
+                for (DocumentId docId : processedDocumentIds) {
+                    for (int i = 0; i < docId.getId().length; i++) {
+                        String pkElement = docId.getId()[i];
+                        ps.setString(i + 1, pkElement);
+                    }
+                    ps.addBatch();
+                }
+                try {
+                    ps.executeBatch();
+                } catch (BatchUpdateException e) {
+                    if (e.getMessage().contains("deadlock detected")) {
+                        log.debug("Database transaction deadlock detected while trying to set the last component. Trying again.");
+                        tryagain = true;
+                    }
+                }
+            } while (tryagain);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            SQLException nextException = e.getNextException();
+            if (null == nextException)
+                throw new AnalysisEngineProcessException(e);
+            else
+                nextException.printStackTrace();
+            throw new AnalysisEngineProcessException(nextException);
+        } finally {
+            processedDocumentIds.clear();
+        }
+    }
+
+}
