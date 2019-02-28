83db5141f3fcd82220d9b4eae9213df38db34352
khituras <chew@gmx.net>
2019-02-28 17:04:10 +0100

    Adding a writer to output acronyms into text format.

---
+++ jcore-acronym-writer/src/test/java/de/julielab/jcore/consumer/acronyms/AcronymWriterTest.java
@@ -0,0 +1,20 @@
+
+package de.julielab.jcore.consumer.acronyms;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Unit tests for jcore-acronym-writer.
+ *
+ */
+public class AcronymWriterTest{
+// TODO
+}
