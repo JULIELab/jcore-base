7be40de48767f01c8ac7c700631d55037eea843c
khituras <chew@gmx.net>
2019-04-12 14:43:45 +0200

    The embedding writer now writes batches of text-sorted embedding files.
    This should make subsequent merging of the files easier.

--- jcore-embedding-writer/src/main/java/de/julielab/jcore/consumer/ew/EmbeddingWriter.java
+++ jcore-embedding-writer/src/main/java/de/julielab/jcore/consumer/ew/EmbeddingWriter.java
@@ -23,9 +23,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
@@ -38,21 +36,30 @@ public class EmbeddingWriter extends JCasAnnotator_ImplBase {
     public static final String PARAM_ANNOTATION_TYPE = "AnnotationType";
     public static final String PARAM_OUTDIR = "OutputDirectory";
     public static final String PARAM_GZIP = "UseGzip";
+    public static final String PARAM_MAX_FILE_ENTRY_SIZE = "MaximumEntriesPerOutputFile";
 
     private final static Logger log = LoggerFactory.getLogger(EmbeddingWriter.class);
     private static int currentConsumerNumber = 0;
     @ConfigurationParameter(name = PARAM_GZIP, mandatory = false, description = "If set to true, the output data will be compressed. Defaults to false.")
     boolean gzip;
-    ByteBuffer bb;
     @ConfigurationParameter(name = PARAM_ANNOTATION_TYPE, mandatory = false, description = "Fully qualified type name to output embeddings for. If an annotation spans multiple tokens, their embeddings are averaged. If this parameter is omitted, the embeddings of all tokens will be written")
     private String annotationType;
-    @ConfigurationParameter(name = PARAM_OUTDIR, description = "The directory into which the embedding files should be written. In a multi-threaded pipeline, each thread writes its own file. The file name will also include the the host name on which it ran.")
+    @ConfigurationParameter(name = PARAM_OUTDIR, description = "The directory into which the embedding files should be written. In a multi-threaded pipeline, each thread writes its own files. The file names will also include the the host name on which it ran. All output files are ordered by tokens or covered annotation text spans. To control the maximum file size, refer to the " + PARAM_MAX_FILE_ENTRY_SIZE + " parameter.")
     private String outputDir;
+    @ConfigurationParameter(name = PARAM_MAX_FILE_ENTRY_SIZE, mandatory = false, description = "The text-embedding pairs are accumulated from multiple CASes before writing them to file. The accumulator keeps the entries sorted by the text part, thus output files are also ordered. This parameter defines the maximum size the accumulate will take before writing its contents to file and clearing itself.", defaultValue = "200000")
+    private int maxEntriesPerFile;
     private String pid;
     private String hostName;
     private int consumerNumber;
     private OutputStream os;
-    private File file;
+    private File nextOutputFile;
+    private int currentBatch;
+    // This ByteBuffer is used to write the current embedding data into
+    private ByteBuffer bb;
+    // We store the embeddings indexed with their text in an ordered fashion. When the maximum size of the
+    // cache is reached, it is output to file. The advantage is that the output files are always ordered
+    // which makes the subsequent merging easier.
+    private TreeMap<String, byte[]> outputCache;
 
     /**
      * This method is called a single time by the framework at component
@@ -63,26 +70,33 @@ public class EmbeddingWriter extends JCasAnnotator_ImplBase {
         annotationType = (String) aContext.getConfigParameterValue(PARAM_ANNOTATION_TYPE);
         outputDir = (String) aContext.getConfigParameterValue(PARAM_OUTDIR);
         gzip = Optional.ofNullable((Boolean) aContext.getConfigParameterValue(PARAM_GZIP)).orElse(false);
+        maxEntriesPerFile = Integer.valueOf(Optional.ofNullable((String)aContext.getConfigParameterValue(PARAM_MAX_FILE_ENTRY_SIZE)).orElse("200000"));
         pid = getPID();
         hostName = getHostName();
         synchronized (PARAM_OUTDIR) {
             consumerNumber = currentConsumerNumber++;
         }
-        file = new File(outputDir + File.separator + "embeddings-" + hostName + "-" + pid + "-writer" + consumerNumber + ".dat" + (gzip ? ".gz" : ""));
-        final File dir = file.getParentFile();
+        currentBatch = 0;
+        nextOutputFile = getNextOutputFile();
+        final File dir = nextOutputFile.getParentFile();
         if (!dir.exists())
             dir.mkdirs();
         try {
-            os = new BufferedOutputStream(new FileOutputStream(file));
+            os = new BufferedOutputStream(new FileOutputStream(nextOutputFile));
             if (gzip)
                 os = new GZIPOutputStream(os);
         } catch (FileNotFoundException e) {
-            log.error("Could not create output stream for the output file {}", file, e);
+            log.error("Could not create output stream for the output file {}", nextOutputFile, e);
             throw new ResourceInitializationException(e);
         } catch (IOException e) {
             log.error("Could not create GZIPOutputStream", e);
             throw new ResourceInitializationException(e);
         }
+        outputCache = new TreeMap<>();
+    }
+
+    private File getNextOutputFile() {
+        return new File(outputDir + File.separator + "embeddings-" + hostName + "-" + pid + "-writer" + consumerNumber + "-batch" + ++currentBatch + ".dat" + (gzip ? ".gz" : ""));
     }
 
     /**
@@ -102,12 +116,14 @@ public class EmbeddingWriter extends JCasAnnotator_ImplBase {
                 JCoReTreeMapAnnotationIndex<Long, Token> tokenIndex = new JCoReTreeMapAnnotationIndex(Comparators.longOverlapComparator(), TermGenerators.longOffsetTermGenerator(), TermGenerators.longOffsetTermGenerator(), aJCas, Token.type);
                 for (Annotation a : aJCas.getAnnotationIndex(type)) {
                     final Stream<Token> overlappingTokens = tokenIndex.search(a);
-                    writeEmbeddingsForAnnotation(overlappingTokens.collect(Collectors.toList()));
+                    cacheEmbeddingsForAnnotation(overlappingTokens.collect(Collectors.toList()));
                 }
             } else {
                 for (Annotation token : aJCas.getAnnotationIndex(Token.type))
-                    writeEmbeddingsForAnnotation(Arrays.asList((Token)token));
+                    cacheEmbeddingsForAnnotation(Arrays.asList((Token)token));
             }
+            if (outputCache.size() >= maxEntriesPerFile)
+                writeEmbeddingsToFile();
         } catch (IOException e) {
             log.error("Could not write to output stream", e);
             throw new AnalysisEngineProcessException(e);
@@ -115,7 +131,14 @@ public class EmbeddingWriter extends JCasAnnotator_ImplBase {
 
     }
 
-    private void writeEmbeddingsForAnnotation(List<Token> tokens) throws IOException {
+    private void writeEmbeddingsToFile() throws IOException {
+        for (byte[] textVector : outputCache.values())
+            os.write(textVector);
+        nextOutputFile = getNextOutputFile();
+        outputCache.clear();
+    }
+
+    private void cacheEmbeddingsForAnnotation(List<Token> tokens) throws IOException {
         final double[] avgEmbedding = getAverageEmbeddingVector(tokens.stream());
         // get the text from the first to the last token
         String text = tokens.get(0).getCAS().getDocumentText().substring(tokens.get(0).getBegin(), tokens.get(tokens.size() - 1).getEnd());
@@ -140,11 +163,12 @@ public class EmbeddingWriter extends JCasAnnotator_ImplBase {
             output = baos.toByteArray();
         }
 
-        os.write(output);
+        byte[] cacheArray = Arrays.copyOf(output, output.length);
+        outputCache.put(text, cacheArray);
     }
 
     private double[] getAverageEmbeddingVector(Stream<Token> tokens) {
-        final MutablePair<Integer, double[]> vectorAvg = tokens.collect(() -> new MutablePair<Integer, double[]>(0, null), (p, t) -> {
+        final MutablePair<Integer, double[]> vectorAvg = tokens.collect(() -> new MutablePair<>(0, null), (p, t) -> {
             p.setLeft(p.getLeft() + 1);
             double[] vector = p.getRight();
             if (vector == null) {
@@ -168,9 +192,15 @@ public class EmbeddingWriter extends JCasAnnotator_ImplBase {
     @Override
     public void collectionProcessComplete() throws AnalysisEngineProcessException {
         try {
+            writeEmbeddingsToFile();
+        } catch (IOException e) {
+            log.error("Exception while writing the last batch of embedding vectors to file {}", nextOutputFile, e);
+            throw new AnalysisEngineProcessException(e);
+        }
+        try {
             os.close();
         } catch (IOException e) {
-            log.error("Exception when closing the output stream to file {}", file, e);
+            log.error("Exception when closing the output stream to file {}", nextOutputFile, e);
             throw new AnalysisEngineProcessException(e);
         }
     }
--- jcore-embedding-writer/src/test/java/de/julielab/jcore/consumer/ew/EmbeddingWriterTest.java
+++ jcore-embedding-writer/src/test/java/de/julielab/jcore/consumer/ew/EmbeddingWriterTest.java
@@ -56,7 +56,9 @@ public class EmbeddingWriterTest {
     @Test
     public void testWriterAllTokens() throws Exception {
         final JCas jCas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-morpho-syntax-types");
-        jCas.setDocumentText("t1 t2 t3");
+        // Note how the tokens are not in ascending text order but in the asserts below, we check in text order.
+        // The output file should be sorted.
+        jCas.setDocumentText("t1 t3 t2");
         Token t1 = new Token(jCas, 0, 2);
         final EmbeddingVector e1 = new EmbeddingVector(jCas, 0, 2);
         final DoubleArray v1 = new DoubleArray(jCas, 2);
@@ -66,22 +68,22 @@ public class EmbeddingWriterTest {
         t1.setEmbeddingVectors(JCoReTools.addToFSArray(null, e1));
         t1.addToIndexes();
 
-        Token t2 = new Token(jCas, 3, 5);
-        final EmbeddingVector e2 = new EmbeddingVector(jCas, 3, 5);
-        final DoubleArray v2 = new DoubleArray(jCas, 2);
-        v2.set(0, 7);
-        v2.set(1, 5);
-        e2.setVector(v2);
-        t2.setEmbeddingVectors(JCoReTools.addToFSArray(null, e2));
-        t2.addToIndexes();
-
-        Token t3 = new Token(jCas, 6, 8);
+        Token t2 = new Token(jCas, 6, 8);
         final EmbeddingVector e3 = new EmbeddingVector(jCas, 6, 8);
         final DoubleArray v3 = new DoubleArray(jCas, 2);
-        v3.set(0, 45);
-        v3.set(1, 13);
+        v3.set(0, 7);
+        v3.set(1, 5);
         e3.setVector(v3);
-        t3.setEmbeddingVectors(JCoReTools.addToFSArray(null, e3));
+        t2.setEmbeddingVectors(JCoReTools.addToFSArray(null, e3));
+        t2.addToIndexes();
+
+        Token t3 = new Token(jCas, 3, 5);
+        final EmbeddingVector e2 = new EmbeddingVector(jCas, 3, 5);
+        final DoubleArray v2 = new DoubleArray(jCas, 2);
+        v2.set(0, 45);
+        v2.set(1, 13);
+        e2.setVector(v2);
+        t3.setEmbeddingVectors(JCoReTools.addToFSArray(null, e2));
         t3.addToIndexes();
 
         final String outputDir = "src/test/resources/output";
@@ -94,6 +96,7 @@ public class EmbeddingWriterTest {
         final File[] files = new File(outputDir).listFiles(file -> file.getName().startsWith("embeddings-"));
         assertThat(files).withFailMessage("There are multiple files in the test output directory, only one file is expected. You can just delete the whole directory " + outputDir).hasSize(1);
         final Pair<List<String>, List<double[]>> embeddings = Decoder.decodeBinaryEmbeddingVectors(new FileInputStream(files[0]));
+        // The output file should be sorted by text
         assertThat(embeddings.getLeft()).containsExactly("t1", "t2", "t3");
         assertThat(embeddings.getRight()).containsExactly(new double[]{3, 12}, new double[]{7, 5}, new double[]{45, 13});
         files[0].delete();
