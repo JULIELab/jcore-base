bbd2e275a132bd2b39208a2fa8a13a47c5ab3016
khituras <chew@gmx.net>
2019-08-19 17:50:35 +0200

    The annotation adder now takes the text annotation format as a parameter.
    Via a new configurable external resource added to the descriptor.

--- jcore-annotation-adder-ae/src/main/java/de/julielab/jcore/ae/annotationadder/annotationsources/InMemoryFileTextAnnotationProvider.java
+++ jcore-annotation-adder-ae/src/main/java/de/julielab/jcore/ae/annotationadder/annotationsources/InMemoryFileTextAnnotationProvider.java
@@ -1,12 +1,21 @@
 package de.julielab.jcore.ae.annotationadder.annotationsources;
 
+import de.julielab.jcore.ae.annotationadder.annotationformat.AnnotationFormat;
 import de.julielab.jcore.ae.annotationadder.annotationformat.SimpleTSVEntityAnnotationFormat;
 import de.julielab.jcore.ae.annotationadder.annotationrepresentations.AnnotationList;
 import de.julielab.jcore.ae.annotationadder.annotationrepresentations.ExternalTextAnnotation;
 import org.apache.uima.resource.DataResource;
 import org.apache.uima.resource.ResourceInitializationException;
+import org.apache.uima.resource.metadata.ConfigurationParameterSettings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
 
 public class InMemoryFileTextAnnotationProvider implements AnnotationProvider<AnnotationList> {
+    public static final String PARAM_ANNOTATION_FORMAT = "AnnotationFormatClass";
+    private final static Logger log = LoggerFactory.getLogger(InMemoryFileTextAnnotationProvider.class);
     private AnnotationSource<AnnotationList> annotationSource;
 
     @Override
@@ -16,11 +25,18 @@ public class InMemoryFileTextAnnotationProvider implements AnnotationProvider<An
 
     @Override
     public void load(DataResource dataResource) throws ResourceInitializationException {
-        // This logic could be made configurable if required so in the future.
-        annotationSource = new FileAnnotationSource(new SimpleTSVEntityAnnotationFormat());
+        final ConfigurationParameterSettings parameterSettings = dataResource.getMetaData().getConfigurationParameterSettings();
+        final String formatClassName = (String) Optional.ofNullable(parameterSettings.getParameterValue(PARAM_ANNOTATION_FORMAT)).orElse(SimpleTSVEntityAnnotationFormat.class.getCanonicalName());
+        AnnotationFormat<ExternalTextAnnotation> format;
+        try {
+            format = (AnnotationFormat<ExternalTextAnnotation>) Class.forName(formatClassName).getDeclaredConstructor().newInstance();
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            log.error("Could not instantiate class {}", formatClassName);
+            throw new ResourceInitializationException(e);
+        }
+        annotationSource = new FileAnnotationSource(format);
         annotationSource.initialize(dataResource);
     }
-
 
 
 }
