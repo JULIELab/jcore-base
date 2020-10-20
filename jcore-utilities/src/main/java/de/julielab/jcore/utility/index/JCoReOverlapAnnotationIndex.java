d4739984e1a7974291818a60b8acb028ea2181b1
khituras <chew@gmx.net>
2020-10-20 09:59:22 +0200

    `JCoReOverlapAnnotationIndex`: When searching, returning a list instead of a stream from that list. Added getters for the internal index lists. Fixes #117.

--- jcore-utilities/src/main/java/de/julielab/jcore/utility/index/JCoReOverlapAnnotationIndex.java
+++ jcore-utilities/src/main/java/de/julielab/jcore/utility/index/JCoReOverlapAnnotationIndex.java
@@ -1,11 +1,10 @@
-/** 
- * 
+/**
  * Copyright (c) 2017, JULIE Lab.
- * All rights reserved. This program and the accompanying materials 
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the BSD-2-Clause License
- *
- * Author: 
- * 
+ * <p>
+ * Author:
+ * <p>
  * Description:
  **/
 package de.julielab.jcore.utility.index;
@@ -19,7 +18,6 @@ import org.apache.uima.jcas.tcas.Annotation;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
 
 /**
  * <p>
@@ -46,145 +44,143 @@ import java.util.stream.Stream;
  * elements. Those are - in the case a lies in the middle of the index elements
  * - n/2.
  * </p>
- * 
- * @author faessler
  *
- * @param <E>
- *            The annotation type the index should be over.
+ * @param <E> The annotation type the index should be over.
+ * @author faessler
  */
 public class JCoReOverlapAnnotationIndex<E extends Annotation> implements JCoReAnnotationIndex<E> {
-	private List<E> beginIndex;
-	private List<E> endIndex;
-	private boolean frozen;
+    private List<E> beginIndex;
+    private List<E> endIndex;
+    private boolean frozen;
 
-	public JCoReOverlapAnnotationIndex() {
-		beginIndex = new ArrayList<>();
-		endIndex = new ArrayList<>();
-	}
+    public JCoReOverlapAnnotationIndex() {
+        beginIndex = new ArrayList<>();
+        endIndex = new ArrayList<>();
+    }
 
-	public JCoReOverlapAnnotationIndex(JCas jcas, int type) {
-		this(jcas, jcas.getCasType(type));
-	}
+    public JCoReOverlapAnnotationIndex(JCas jcas, int type) {
+        this(jcas, jcas.getCasType(type));
+    }
 
-	public JCoReOverlapAnnotationIndex(JCas jcas, Type type) {
-		this();
-		index(jcas, type);
-		freeze();
-	}
+    public JCoReOverlapAnnotationIndex(JCas jcas, Type type) {
+        this();
+        index(jcas, type);
+        freeze();
+    }
 
-	/**
-	 * Indexes the whole contents of the CAS annotation index of type
-	 * <tt>type</tt>. For each annotation, the {@link #indexTermGenerator} is
-	 * used to create terms with which the annotation will be associated in the
-	 * index and can be retrieved by a <code>search</code> method.
-	 * 
-	 * @param jCas
-	 *            A CAS instance.
-	 * @param type
-	 *            The annotation type to index.
-	 */
-	public void index(JCas jCas, int type) {
-		index(jCas, jCas.getCasType(type));
-	}
+    public void index(JCas jCas, int type) {
+        index(jCas, jCas.getCasType(type));
+    }
 
-	/**
-	 * Indexes the whole contents of the CAS annotation index of type
-	 * <tt>type</tt>. For each annotation, the {@link #indexTermGenerator} is
-	 * used to create terms with which the annotation will be associated in the
-	 * index and can be retrieved by a <code>search</code> method.
-	 * 
-	 * @param jCas
-	 *            A CAS instance.
-	 * @param type
-	 *            The annotation type to index.
-	 */
-	@SuppressWarnings("unchecked")
-	public void index(JCas jCas, Type type) {
-		FSIterator<Annotation> it = jCas.getAnnotationIndex(type).iterator();
-		while (it.hasNext()) {
-			Annotation annotation = (Annotation) it.next();
-			index((E) annotation);
-		}
-	}
+    public void index(JCas jCas, Type type) {
+        FSIterator<Annotation> it = jCas.getAnnotationIndex(type).iterator();
+        while (it.hasNext()) {
+            Annotation annotation = it.next();
+            index((E) annotation);
+        }
+    }
 
-	public void index(E annotation) {
-		if (frozen)
-			throw new IllegalStateException("This index is frozen and cannot except further items.");
-		beginIndex.add(annotation);
-		endIndex.add(annotation);
-	}
+    public void index(E annotation) {
+        if (frozen)
+            throw new IllegalStateException("This index is frozen and cannot accept further items.");
+        beginIndex.add(annotation);
+        endIndex.add(annotation);
+    }
 
-	public void freeze() {
-		frozen = true;
-		Collections.sort(beginIndex, Comparators.beginOffsetComparator());
-		Collections.sort(endIndex, Comparators.endOffsetComparator());
-	}
+    public void freeze() {
+        frozen = true;
+        Collections.sort(beginIndex, Comparators.beginOffsetComparator());
+        Collections.sort(endIndex, Comparators.endOffsetComparator());
+    }
 
-	/**
-	 * Returns all annotation in the index overlapping in any way with a
-	 * (embedded, covering, partial overlappings). The resulting list is either
-	 * sorted by begin or end offset. It is not easily predictable which case it
-	 * is (could be added as a return value if that would be useful in any way).
-	 * 
-	 * @param a
-	 *            The annotation to retrieve overlapping annotations from the
-	 *            index for.
-	 * @return All annotations in the index overlapping a.
-	 */
-	public <T extends Annotation> Stream<E> search(T a) {
-		if (!frozen)
-			throw new IllegalStateException(
-					"This index is not frozen and cannot be used yet. Freeze the index before searching.");
-		if (beginIndex.isEmpty())
-			return Stream.empty();
-		// The following is rather difficult to understand from the code. The
-		// idea is the following:
-		// We search annotations overlapping with a. Thus, we can rule out those
-		// annotations that end before a or start after a.
-		// In the next 4 lines, we determine how many annotations can be ruled
-		// out because they start after a and how many end before a.
-		int begin = a.getBegin();
-		int end = a.getEnd();
-		int indexBeginAfterEnd = insertionPoint(JCoReTools.binarySearch(beginIndex, an -> an.getBegin(), end));
-		int indexEndBeforeBegin = insertionPoint(JCoReTools.binarySearch(endIndex, an -> an.getEnd(), begin));
+    /**
+     * Returns all annotation in the index overlapping in any way with a
+     * (embedded, covering, partial overlappings). The resulting list is either
+     * sorted by begin or end offset. It is not easily predictable which case it
+     * is (could be added as a return value if that would be useful in any way).
+     *
+     * @param a The annotation to retrieve overlapping annotations from the
+     *          index for.
+     * @return All annotations in the index overlapping a.
+     */
+    public <T extends Annotation> List<E> search(T a) {
+        if (!frozen)
+            throw new IllegalStateException(
+                    "This index is not frozen and cannot be used yet. Freeze the index before searching.");
+        if (beginIndex.isEmpty())
+            return Collections.emptyList();
+        // The following is rather difficult to understand from the code. The
+        // idea is the following:
+        // We search annotations overlapping with a. Thus, we can rule out those
+        // annotations that end before a or start after a.
+        // In the next 4 lines, we determine how many annotations can be ruled
+        // out because they start after a and how many end before a.
+        int begin = a.getBegin();
+        int end = a.getEnd();
+        int indexBeginAfterEnd = insertionPoint(JCoReTools.binarySearch(beginIndex, an -> an.getBegin(), end));
+        int indexEndBeforeBegin = insertionPoint(JCoReTools.binarySearch(endIndex, an -> an.getEnd(), begin));
 
-		// Depending on which case rules out more annotations - ending before a
-		// or starting after a - we look at the case that leaves us with the
-		// fewest annotations. If those were the annotations that started after
-		// a, then we keep those that start before a ends. Those are than
-		// filtered for annotations that end before a starts.
-		if (indexBeginAfterEnd < endIndex.size() - indexEndBeforeBegin) {
-			List<E> beginBeforeEnd = new ArrayList<>(beginIndex.subList(0, indexBeginAfterEnd));
-			ArrayList<E> result = new ArrayList<>();
-			for (E e : beginBeforeEnd) {
-				if (e.getEnd() > begin)
-					result.add(e);
-			}
-			return result.stream();
-		} else {
-			List<E> endAfterBegin = new ArrayList<>(endIndex.subList(indexEndBeforeBegin, endIndex.size()));
-			ArrayList<E> result = new ArrayList<>();
-			for (E e : endAfterBegin) {
-				if (e.getBegin() < end)
-					result.add(e);
-			}
-			return result.stream();
-		}
-	}
+        // Depending on which case rules out more annotations - ending before a
+        // or starting after a - we look at the case that leaves us with the
+        // fewest annotations. If those were the annotations that started after
+        // a, then we keep those that start before a ends. Those are than
+        // filtered for annotations that end before a starts.
+        if (indexBeginAfterEnd < endIndex.size() - indexEndBeforeBegin) {
+            List<E> beginBeforeEnd = new ArrayList<>(beginIndex.subList(0, indexBeginAfterEnd));
+            List<E> result = new ArrayList<>();
+            for (E e : beginBeforeEnd) {
+                if (e.getEnd() > begin)
+                    result.add(e);
+            }
+            return result;
+        } else {
+            List<E> endAfterBegin = new ArrayList<>(endIndex.subList(indexEndBeforeBegin, endIndex.size()));
+            List<E> result = new ArrayList<>();
+            for (E e : endAfterBegin) {
+                if (e.getBegin() < end)
+                    result.add(e);
+            }
+            return result;
+        }
+    }
 
-	private int insertionPoint(int i) {
-		return i < 0 ? -(i + 1) : i;
-	}
+    private int insertionPoint(int i) {
+        return i < 0 ? -(i + 1) : i;
+    }
 
-	/**
-	 * Un-freeze the index to allow new elements to be added.
-	 */
-	public void melt() {
-		frozen = false;
-	}
+    /**
+     * Un-freeze the index to allow new elements to be added.
+     */
+    public void melt() {
+        frozen = false;
+    }
 
-	@Override
-	public void add(E a) {
-		index(a);
-	}
+    @Override
+    public void add(E a) {
+        index(a);
+    }
+
+    /**
+     * <p>Returns the internal list where the indexed annotations are sorted by begin offset. External changes to
+     * this list might break the index.</p>
+     *
+     * @return The indexed annotations sorted bei their begin offset.
+     */
+    public List<E> getBeginIndex() {
+        return beginIndex;
+    }
+
+    /**
+     * <p>Returns the internal list where the indexed annotations are sorted by end offset. External changes to
+     * this list might break the index.</p>
+     *
+     * @return The indexed annotations sorted bei their end offset.
+     */
+    public List<E> getEndIndex() {
+        return endIndex;
+    }
+
+    public boolean isFrozen() {
+        return frozen;
+    }
 }
