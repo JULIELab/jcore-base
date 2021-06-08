2317cfa0011351f23ea6df834b068daf7fe95b93
khituras <chew@gmx.net>
2021-06-08 15:17:41 +0200

    Adding trace-level logging to the `AnnotationDefinedFlow` in order to see the flow when actually running all components together.

--- jcore-flow-controllers/src/main/java/de/julielab/jcore/flow/annotationdefined/AnnotationDefinedFlow.java
+++ jcore-flow-controllers/src/main/java/de/julielab/jcore/flow/annotationdefined/AnnotationDefinedFlow.java
@@ -1,14 +1,18 @@
 package de.julielab.jcore.flow.annotationdefined;
 
 import de.julielab.jcore.types.casflow.ToVisit;
+import de.julielab.jcore.utility.JCoReTools;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.analysis_engine.metadata.FixedFlow;
 import org.apache.uima.analysis_engine.metadata.FlowConstraints;
+import org.apache.uima.cas.CASException;
 import org.apache.uima.flow.FinalStep;
 import org.apache.uima.flow.JCasFlow_ImplBase;
 import org.apache.uima.flow.SimpleStep;
 import org.apache.uima.flow.Step;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * <p>Returns steps according an existing {@link ToVisit} annotation of the CAS or, if not present, the default aggregate flow.</p>
@@ -16,6 +20,7 @@ import org.jetbrains.annotations.Nullable;
  * the currently read document already exists in the database.</p>
  */
 public class AnnotationDefinedFlow extends JCasFlow_ImplBase {
+    private final static Logger log = LoggerFactory.getLogger(AnnotationDefinedFlow.class);
     private String[] toVisitKeys;
     private String[] fixedFlow;
     private int currentPos;
@@ -24,7 +29,8 @@ public class AnnotationDefinedFlow extends JCasFlow_ImplBase {
      * <p>Creates a flow that follows to entries in {@link ToVisit#getDelegateKeys()} of <tt>toVisit</tt> or, if
      * <tt>toVisit</tt> is null, falls back to the default fixed flow.</p>
      * <p>If <tt>toVisit</tt> is not null but the <tt>delegateKeys</tt> are null or empty, no component in the aggregate using this flow will process the respective CAS.</p>
-     * @param toVisit An annotation containing the keys of the delegate AEs to visit. May be null which case the default fixed flow will be used.
+     *
+     * @param toVisit         An annotation containing the keys of the delegate AEs to visit. May be null which case the default fixed flow will be used.
      * @param flowConstraints The default fixed flow of the aggregate analysis engine.
      * @throws AnalysisEngineProcessException If <tt>flowConstraints</tt> is not a fixed flow.
      */
@@ -36,7 +42,21 @@ public class AnnotationDefinedFlow extends JCasFlow_ImplBase {
         // 1. There are given keys to visit, use them.
         // 2. There are no keys given but the ToVisit annotation is not null, skip all components.
         // 3. There is not ToVisit annotation at all, use the default fixed flow.
-        if(toVisit != null && toVisit.getDelegateKeys() != null)
+        if (log.isTraceEnabled()) {
+            try {
+            String docId = JCoReTools.getDocId(toVisit.getCAS().getJCas());
+                if (toVisit != null) {
+                    String[] delegateKeys = toVisit.getDelegateKeys() != null ? toVisit.getDelegateKeys().toArray() : null;
+                    log.trace("Found ToVisit annotation for document {} with the following component keys: {}", docId, delegateKeys);
+                } else {
+                    log.trace("Got no ToVisit annotation for document {}.", docId);
+                }
+            } catch (CASException e) {
+                log.error("Could not retrieve JCas from ToVisit annotation.", e);
+                throw new AnalysisEngineProcessException(e);
+            }
+        }
+        if (toVisit != null && toVisit.getDelegateKeys() != null)
             toVisitKeys = toVisit.getDelegateKeys().toArray();
         else if (toVisit != null)
             toVisitKeys = new String[0];
