e038a6340a3cb91d4ee06388b94b07d71ba49553
khituras <chew@gmx.net>
2021-07-07 16:11:27 +0200

    Fixing offset issues with JCoReCondensedDocumentText.
    Converting original offsets to condensed offsets when the original offset lied in a cut away area was not working correctly. In fact, the case was not even covered in the code.

--- jcore-utilities/src/main/java/de/julielab/jcore/utility/JCoReCondensedDocumentText.java
+++ jcore-utilities/src/main/java/de/julielab/jcore/utility/JCoReCondensedDocumentText.java
@@ -13,169 +13,196 @@ import java.util.TreeMap;
  * document text that results from cutting out said text passages. It offers a
  * method to return the actual text string and a method to map the character
  * offsets of the compacted string to the original CAS document text.
- * 
- * @author faessler
  *
+ * @author faessler
  */
 public class JCoReCondensedDocumentText {
-	private NavigableMap<Integer, Integer> condensedPos2SumCutMap;
-	private NavigableMap<Integer, Integer> originalPos2SumCutMap;
-	private String condensedText;
-	private JCas cas;
-	private Set<Character> cutAwayFillCharacters;
+    private NavigableMap<Integer, Integer> condensedPos2SumCutMap;
+    private NavigableMap<Integer, Integer> originalPos2SumCutMap;
+    private String condensedText;
+    private JCas cas;
+    private Set<Character> cutAwayFillCharacters;
 
-	public JCas getCas() {
-		return cas;
-	}
-	/**
-	 * <p>
-	 * Cuts away the covered text of annotations of a type in <tt>cutAwayTypes</tt>
-	 * from the <tt>cas</tt> document text. If <tt>cutAwayTypes</tt> is null or
-	 * empty, this class' methods will return the original CAS data.
-	 * </p>
-	 *
-	 * @param cas
-	 *            The CAS for which the document text should be cut.
-	 * @param cutAwayTypes
-	 *            The types for cutting. May be null.
-	 * @throws ClassNotFoundException
-	 *             If <tt>cutAwayTypes</tt> contains non-existing type names.
-	 */
-	public JCoReCondensedDocumentText(JCas cas, Set<String> cutAwayTypes) throws ClassNotFoundException {
-		this(cas, cutAwayTypes, null);
-	}
+    /**
+     * <p>
+     * Cuts away the covered text of annotations of a type in <tt>cutAwayTypes</tt>
+     * from the <tt>cas</tt> document text. If <tt>cutAwayTypes</tt> is null or
+     * empty, this class' methods will return the original CAS data.
+     * </p>
+     *
+     * @param cas          The CAS for which the document text should be cut.
+     * @param cutAwayTypes The types for cutting. May be null.
+     * @throws ClassNotFoundException If <tt>cutAwayTypes</tt> contains non-existing type names.
+     */
+    public JCoReCondensedDocumentText(JCas cas, Set<String> cutAwayTypes) throws ClassNotFoundException {
+        this(cas, cutAwayTypes, null);
+    }
 
-	/**
-	 * <p>
-	 * Cuts away the covered text of annotations of a type in <tt>cutAwayTypes</tt>
-	 * from the <tt>cas</tt> document text. If <tt>cutAwayTypes</tt> is null or
-	 * empty, this class' methods will return the original CAS data.
-	 * </p>
-	 * <p>The <tt>cutAwayFillCharacters</tt> set may provide characters that, when being the only character between
-	 * to cut-away annotations, will add to the span of text being cut away. This way, enumerations of references
-	 * (e.g. "4,6,8") can be completely removed, for example.</p>
-	 * 
-	 * @param cas
-	 *            The CAS for which the document text should be cut.
-	 * @param cutAwayTypes
-	 *            The types for cutting. May be null.
-	 * @param cutAwayFillCharacters Characters that, when being the only separator between two cut away annotations, are also cut away.
-	 * @throws ClassNotFoundException
-	 *             If <tt>cutAwayTypes</tt> contains non-existing type names.
-	 */
-	public JCoReCondensedDocumentText(JCas cas, Set<String> cutAwayTypes, Set<Character> cutAwayFillCharacters) throws ClassNotFoundException {
-		this.cas = cas;
-		this.cutAwayFillCharacters = cutAwayFillCharacters;
-		buildMap(cas, cutAwayTypes);
-	}
+    /**
+     * <p>
+     * Cuts away the covered text of annotations of a type in <tt>cutAwayTypes</tt>
+     * from the <tt>cas</tt> document text. If <tt>cutAwayTypes</tt> is null or
+     * empty, this class' methods will return the original CAS data.
+     * </p>
+     * <p>The <tt>cutAwayFillCharacters</tt> set may provide characters that, when being the only character between
+     * to cut-away annotations, will add to the span of text being cut away. This way, enumerations of references
+     * (e.g. "4,6,8") can be completely removed, for example.</p>
+     *
+     * @param cas                   The CAS for which the document text should be cut.
+     * @param cutAwayTypes          The types for cutting. May be null.
+     * @param cutAwayFillCharacters Characters that, when being the only separator between two cut away annotations, are also cut away.
+     * @throws ClassNotFoundException If <tt>cutAwayTypes</tt> contains non-existing type names.
+     */
+    public JCoReCondensedDocumentText(JCas cas, Set<String> cutAwayTypes, Set<Character> cutAwayFillCharacters) throws ClassNotFoundException {
+        this.cas = cas;
+        this.cutAwayFillCharacters = cutAwayFillCharacters;
+        buildMap(cas, cutAwayTypes);
+    }
 
-	/**
-	 * <p>
-	 * Creates a map that maps those positions of the small-cut text that correspond
-	 * to an intermediate next position after a cut-away annotation in the original
-	 * text to the sum of ranges covered by cut-away annotations up to the original
-	 * offset.
-	 * </p>
-	 * <p>
-	 * If <tt>cutAwayTypes</tt> is empty, no work will be done and the methods of
-	 * this class we return the original text and offets of the CAS.
-	 * </p>
-	 * 
-	 * @param cas
-	 *            The CAS for create a cut-away document text for.
-	 * @param cutAwayTypes
-	 *            The qualified type names of the annotations whose covered text
-	 *            should be cut away.
-	 * @throws ClassNotFoundException
-	 *             If <tt>cutAwayTypes</tt> contains type identifiers to
-	 *             non-existing types.
-	 */
-	public void buildMap(JCas cas, Set<String> cutAwayTypes) throws ClassNotFoundException {
-		if (cutAwayTypes == null || cutAwayTypes.isEmpty())
-			return;
-		StringBuilder sb = new StringBuilder();
-		condensedPos2SumCutMap = new TreeMap<>();
-		condensedPos2SumCutMap.put(0, 0);
-		originalPos2SumCutMap = new TreeMap<>();
-		originalPos2SumCutMap.put(0, 0);
-		JCoReAnnotationIndexMerger merger = new JCoReAnnotationIndexMerger(cutAwayTypes, true, null, cas);
-		int cutSum = 0;
-		int lastBegin = 0;
-		int lastEnd = -1;
-		// For each ignored annotation, there could be following annotations overlapping
-		// with the first, effectively enlarging the ignored span. Thus, we iterate
-		// until we find an ignored annotation that has a positive (not 0) distance to a
-		// previous one. Then, we store the length of the span of cut-away annotations
-		// for the largest end of the previous annotations.
-		while (merger.incrementAnnotation()) {
-			int end = merger.getCurrentEnd();
-			int begin = merger.getCurrentBegin();
+    public JCas getCas() {
+        return cas;
+    }
 
-			boolean moreThanOneCharacterDistance = begin - lastEnd > 2;
-			boolean previousCharacterIsCutAwayDelimiter = cutAwayFillCharacters == null || cutAwayFillCharacters.isEmpty() || (begin - lastEnd == 2 && cutAwayFillCharacters.contains(cas.getDocumentText().charAt(begin - 1)));
-			if (lastEnd > 0 && begin > lastEnd && (previousCharacterIsCutAwayDelimiter || moreThanOneCharacterDistance)) {
-				cutSum += lastEnd - lastBegin;
-				int condensedPosition = lastEnd - cutSum + 1;
-				condensedPos2SumCutMap.put(condensedPosition, cutSum);
-				originalPos2SumCutMap.put(lastEnd, cutSum);
-				lastBegin = begin;
-				sb.append(cas.getDocumentText(), lastEnd, begin);
-			} else if (lastEnd < 0) {
-				lastBegin = begin;
-				sb.append(cas.getDocumentText(), 0, begin);
-			}
-			lastEnd = end;
-		}
-		// Since we iterate one annotation further than the annotation we store the span
-		// for, we need to take care of the very last ignored annotation after the loop
-		// - it has never been handled itself.
-		if (lastEnd > 0) {
-			cutSum += lastEnd - lastBegin;
-			int condensedPosition = lastEnd - cutSum + 1;
-			condensedPos2SumCutMap.put(condensedPosition, cutSum);
-			originalPos2SumCutMap.put(lastEnd, cutSum);
-		}
-		// If lastEnd is still -1, we just did not find any of the cut away annotations. Thus, we just copy the whole text.
-		if (lastEnd == -1)
-		    lastEnd = 0;
-		if (lastEnd < cas.getDocumentText().length())
-			sb.append(cas.getDocumentText().substring(lastEnd));
-		condensedText = sb.toString();
-	}
+    /**
+     * <p>
+     * Creates a map that maps those positions of the small-cut text that correspond
+     * to an intermediate next position after a cut-away annotation in the original
+     * text to the sum of ranges covered by cut-away annotations up to the original
+     * offset.
+     * </p>
+     * <p>
+     * If <tt>cutAwayTypes</tt> is empty, no work will be done and the methods of
+     * this class we return the original text and offets of the CAS.
+     * </p>
+     *
+     * @param cas          The CAS for create a cut-away document text for.
+     * @param cutAwayTypes The qualified type names of the annotations whose covered text
+     *                     should be cut away.
+     * @throws ClassNotFoundException If <tt>cutAwayTypes</tt> contains type identifiers to
+     *                                non-existing types.
+     */
+    public void buildMap(JCas cas, Set<String> cutAwayTypes) throws ClassNotFoundException {
+        if (cutAwayTypes == null || cutAwayTypes.isEmpty())
+            return;
+        StringBuilder sb = new StringBuilder();
+        condensedPos2SumCutMap = new TreeMap<>();
+        condensedPos2SumCutMap.put(0, 0);
+        originalPos2SumCutMap = new TreeMap<>();
+        originalPos2SumCutMap.put(0, 0);
+        JCoReAnnotationIndexMerger merger = new JCoReAnnotationIndexMerger(cutAwayTypes, true, null, cas);
+        int cutSum = 0;
+        int lastBegin = 0;
+        int lastEnd = -1;
+        int lastCutSum = 0;
+        // For each ignored annotation, there could be following annotations overlapping
+        // with the first, effectively enlarging the ignored span. Thus, we iterate
+        // until we find an ignored annotation that has a positive (not 0) distance to a
+        // previous one. Then, we store the length of the span of cut-away annotations
+        // for the largest end of the previous annotations.
+        while (merger.incrementAnnotation()) {
+            int begin = merger.getCurrentBegin();
+            int end = merger.getCurrentEnd();
 
-	/**
-	 * Given a character offset relative to the condensed document text, this method
-	 * returns the corresponding offset in the original CAS document text.
-	 * 
-	 * @param condensedOffset
-	 *            The character offset in the condensed document text string.
-	 * @return The character offset relative to the original CAS document text
-	 *         associated with <tt>condensedOffset</tt>.
-	 */
-	public int getOriginalOffsetForCondensedOffset(int condensedOffset) {
-		if (condensedPos2SumCutMap == null)
-			return condensedOffset;
-		Entry<Integer, Integer> floorEntry = condensedPos2SumCutMap.floorEntry(condensedOffset);
-		return condensedOffset + floorEntry.getValue();
-	}
-	
-	/**
-	 * Given a character offset relative to the original CAS document text, this method
-	 * returns the corresponding offset in the condensed document text.
-	 * 
-	 * @param originalOffset
-	 *            The character offset in the originalOffset document CAS text string.
-	 * @return The character offset relative to the condensed document text
-	 *         associated with <tt>originalOffset</tt>.
-	 */
-	public int getCondensedOffsetForOriginalOffset(int originalOffset) {
-		if (originalPos2SumCutMap == null)
-			return originalOffset;
-		Entry<Integer, Integer> floorEntry = originalPos2SumCutMap.floorEntry(originalOffset);
-		return originalOffset - floorEntry.getValue();
-	}
+            boolean moreThanOneCharacterDistance = begin - lastEnd > 2;
+            boolean previousCharacterIsCutAwayDelimiter = cutAwayFillCharacters == null || cutAwayFillCharacters.isEmpty() || (begin - lastEnd == 2 && cutAwayFillCharacters.contains(cas.getDocumentText().charAt(begin - 1)));
+            if (lastEnd > 0 && begin > lastEnd && (previousCharacterIsCutAwayDelimiter || moreThanOneCharacterDistance)) {
+                // Adapt offsets to remove superfluous white spaces from the condensed text
+                boolean precedingCharacterIsWS = lastBegin == 0 || Character.isWhitespace(cas.getDocumentText().charAt(lastBegin - 1));
+                boolean succeedingCharacterIsWS = lastEnd < cas.getDocumentText().length() && Character.isWhitespace(cas.getDocumentText().charAt(lastEnd));
+                if (precedingCharacterIsWS && succeedingCharacterIsWS)
+                    ++lastEnd;
+                if (precedingCharacterIsWS && end >= cas.getDocumentText().length())
+                    --begin;
+                // The current cut away annotation begins after the previous cut away annotation, thus there is no
+                // overlap and we can add the current state to the maps.
+                cutSum += lastEnd - lastBegin;
+                int condensedPosition = lastEnd - cutSum + 1;
+                condensedPos2SumCutMap.put(condensedPosition, cutSum);
+                // For original offsets we need to be able to know where the begin and the end of
+                // the cut away annotation was. This is exploited in getCondensedOffsetForOriginalOffset()
+                originalPos2SumCutMap.put(lastBegin, lastCutSum);
+                originalPos2SumCutMap.put(lastEnd, cutSum);
+                lastBegin = begin;
+                lastCutSum = cutSum;
+                sb.append(cas.getDocumentText(), lastEnd, begin);
+            } else if (lastEnd < 0) {
+                // This is the first annotation
+                if (begin > 0 && end >= cas.getDocumentText().length() && Character.isWhitespace(cas.getDocumentText().charAt(begin - 1)))
+                    // Case: A single cut away annotation right at the end of the document text.
+                    // Then we want to extend the cut away area to the leading whitespace to remove that as well.
+                    --begin;
+                lastBegin = begin;
+                sb.append(cas.getDocumentText(), 0, begin);
+            }
+            lastEnd = end;
+        }
+        // Since we iterate one annotation further than the annotation we store the span
+        // for, we need to take care of the very last ignored annotation after the loop
+        // - it has never been handled itself.
+        if (lastEnd > 0) {
+            // Adapt offsets to avoid unnecessary white spaces regarding the tail of the document text.
+            boolean precedingCharacterIsWS = lastBegin < 1 || Character.isWhitespace(cas.getDocumentText().charAt(lastBegin - 1));
+            boolean succeedingCharacterIsWS = lastEnd < cas.getDocumentText().length() && Character.isWhitespace(cas.getDocumentText().charAt(lastEnd));
+            if (precedingCharacterIsWS && (succeedingCharacterIsWS || lastEnd >= cas.getDocumentText().length()))
+                ++lastEnd;
+            cutSum += lastEnd - lastBegin;
+            int condensedPosition = lastEnd - cutSum + 1;
+            condensedPos2SumCutMap.put(condensedPosition, cutSum);
+            originalPos2SumCutMap.put(lastBegin, lastCutSum);
+            originalPos2SumCutMap.put(lastEnd, cutSum);
+        }
+        // If lastEnd is still -1, we just did not find any of the cut away annotations. Thus, we just copy the whole text.
+        if (lastEnd == -1)
+            lastEnd = 0;
+        if (lastEnd < cas.getDocumentText().length())
+            sb.append(cas.getDocumentText().substring(lastEnd));
+        condensedText = sb.toString();
+    }
 
-	public String getCodensedText() {
-		return condensedText != null ? condensedText : cas.getDocumentText();
-	}
+
+    /**
+     * Given a character offset relative to the condensed document text, this method
+     * returns the corresponding offset in the original CAS document text.
+     *
+     * @param condensedOffset The character offset in the condensed document text string.
+     * @return The character offset relative to the original CAS document text
+     * associated with <tt>condensedOffset</tt>.
+     */
+    public int getOriginalOffsetForCondensedOffset(int condensedOffset) {
+        if (condensedPos2SumCutMap == null)
+            return condensedOffset;
+        Entry<Integer, Integer> floorEntry = condensedPos2SumCutMap.floorEntry(condensedOffset);
+        return condensedOffset + floorEntry.getValue();
+    }
+
+    /**
+     * Given a character offset relative to the original CAS document text, this method
+     * returns the corresponding offset in the condensed document text.
+     *
+     * @param originalOffset The character offset in the originalOffset document CAS text string.
+     * @return The character offset relative to the condensed document text
+     * associated with <tt>originalOffset</tt>.
+     */
+    public int getCondensedOffsetForOriginalOffset(int originalOffset) {
+        if (originalPos2SumCutMap == null)
+            return originalOffset;
+        Entry<Integer, Integer> floorEntry = originalPos2SumCutMap.floorEntry(originalOffset);
+        Entry<Integer, Integer> ceilingEntry = originalPos2SumCutMap.ceilingEntry(originalOffset);
+        // floor entry can never be null because the mapping 0=0 always exists
+        if (floorEntry != null && ceilingEntry != null) {
+            // Determine if the original offset is inside or outside of a cut away annotation.
+            // If the difference of key and value is the same for floor and ceiling, the originalOffset
+            // is within of a cut away annotation. Otherwise, it is outside a cut away annotation
+            int floorDiff = floorEntry.getKey() - floorEntry.getValue();
+            int ceilingDiff = ceilingEntry.getKey() - ceilingEntry.getValue();
+            boolean withinCutAway = floorDiff == ceilingDiff;
+            if (withinCutAway)
+                return originalOffset - ceilingEntry.getValue() + (ceilingEntry.getKey() - originalOffset);
+        }
+        return originalOffset - floorEntry.getValue();
+    }
+
+    public String getCodensedText() {
+        return condensedText != null ? condensedText : cas.getDocumentText();
+    }
 }
--- jcore-utilities/src/test/java/de/julielab/jcore/utility/JCoReCondensedDocumentTextTest.java
+++ jcore-utilities/src/test/java/de/julielab/jcore/utility/JCoReCondensedDocumentTextTest.java
@@ -1,5 +1,6 @@
 package de.julielab.jcore.utility;
 
+import de.julielab.jcore.types.Annotation;
 import de.julielab.jcore.types.InternalReference;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.jcas.JCas;
@@ -82,5 +83,93 @@ public class JCoReCondensedDocumentTextTest {
 		JCoReCondensedDocumentText condensedText = new JCoReCondensedDocumentText(jcas,
 				new HashSet<>(Arrays.asList(InternalReference.class.getCanonicalName())), Set.of(',', ';'));
 		assertEquals("This sentence has multiple references. This is a second sentence.", condensedText.getCodensedText());
+	}
+
+	@Test
+	public void testCondensedOffsetsWithinCutawayAnnotations() throws Exception {
+		JCas jcas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-morpho-syntax-types",
+				"de.julielab.jcore.types.jcore-document-structure-types");
+		jcas.setDocumentText("Not cut away 1. Cut away 1. Not cut away 2. Cut away 2. Not cut away 3.");
+		Annotation cutAwayAnnotation = new Annotation(jcas, 16, 27);
+		cutAwayAnnotation.addToIndexes();
+		Annotation cutAwayAnnotation2 = new Annotation(jcas, 44, 55);
+		cutAwayAnnotation2.addToIndexes();
+
+		JCoReCondensedDocumentText condensedText = new JCoReCondensedDocumentText(jcas,
+				new HashSet<>(Arrays.asList(Annotation.class.getCanonicalName())));
+		assertEquals("Not cut away 1. Not cut away 2. Not cut away 3.", condensedText.getCodensedText());
+		assertEquals(10, condensedText.getCondensedOffsetForOriginalOffset(10));
+		assertEquals(15, condensedText.getCondensedOffsetForOriginalOffset(15));
+		assertEquals(16, condensedText.getCondensedOffsetForOriginalOffset(16));
+		assertEquals(16, condensedText.getCondensedOffsetForOriginalOffset(17));
+		assertEquals(16, condensedText.getCondensedOffsetForOriginalOffset(27));
+		assertEquals(19, condensedText.getCondensedOffsetForOriginalOffset(31));
+	}
+
+	@Test
+	public void testCutAwayAtBeginning() throws Exception {
+		JCas jcas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-morpho-syntax-types",
+				"de.julielab.jcore.types.jcore-document-structure-types");
+		jcas.setDocumentText("Cut away. Not cut away.");
+		Annotation cutAwayAnnotation = new Annotation(jcas, 0, 9);
+		cutAwayAnnotation.addToIndexes();
+
+		JCoReCondensedDocumentText condensedText = new JCoReCondensedDocumentText(jcas,
+				new HashSet<>(Arrays.asList(Annotation.class.getCanonicalName())));
+		assertEquals("Not cut away.", condensedText.getCodensedText());
+		assertEquals(0, condensedText.getCondensedOffsetForOriginalOffset(3));
+		assertEquals(3, condensedText.getCondensedOffsetForOriginalOffset(13));
+	}
+
+	@Test
+	public void testCutAwayAtEnd() throws Exception {
+		JCas jcas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-morpho-syntax-types",
+				"de.julielab.jcore.types.jcore-document-structure-types");
+		jcas.setDocumentText("Not cut away. Cut away.");
+		Annotation cutAwayAnnotation = new Annotation(jcas, 14, 23);
+		cutAwayAnnotation.addToIndexes();
+
+		JCoReCondensedDocumentText condensedText = new JCoReCondensedDocumentText(jcas,
+				new HashSet<>(Arrays.asList(Annotation.class.getCanonicalName())));
+		assertEquals("Not cut away.", condensedText.getCodensedText());
+		assertEquals(10, condensedText.getCondensedOffsetForOriginalOffset(10));
+		assertEquals(13, condensedText.getCondensedOffsetForOriginalOffset(16));
+		assertEquals(13, condensedText.getCondensedOffsetForOriginalOffset(23));
+	}
+
+	@Test
+	public void testEmbeddedCutAway() throws Exception {
+		JCas jcas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-morpho-syntax-types",
+				"de.julielab.jcore.types.jcore-document-structure-types");
+		jcas.setDocumentText("Not cut away. Cut away. Not cut away.");
+		Annotation cutAwayAnnotation = new Annotation(jcas, 14, 23);
+		cutAwayAnnotation.addToIndexes();
+
+		JCoReCondensedDocumentText condensedText = new JCoReCondensedDocumentText(jcas,
+				new HashSet<>(Arrays.asList(Annotation.class.getCanonicalName())));
+		assertEquals("Not cut away. Not cut away.", condensedText.getCodensedText());
+		assertEquals(10, condensedText.getCondensedOffsetForOriginalOffset(10));
+		assertEquals(14, condensedText.getCondensedOffsetForOriginalOffset(16));
+		assertEquals(14, condensedText.getCondensedOffsetForOriginalOffset(23));
+		assertEquals(15, condensedText.getCondensedOffsetForOriginalOffset(25));
+	}
+
+	@Test
+	public void testEnclosingCutAway() throws Exception {
+		JCas jcas = JCasFactory.createJCas("de.julielab.jcore.types.jcore-morpho-syntax-types",
+				"de.julielab.jcore.types.jcore-document-structure-types");
+		jcas.setDocumentText("Cut away. Not cut away. Cut away.");
+		Annotation cutAwayAnnotation = new Annotation(jcas, 0, 9);
+		cutAwayAnnotation.addToIndexes();
+		Annotation cutAwayAnnotation2 = new Annotation(jcas, 24, 33);
+		cutAwayAnnotation2.addToIndexes();
+
+		JCoReCondensedDocumentText condensedText = new JCoReCondensedDocumentText(jcas,
+				new HashSet<>(Arrays.asList(Annotation.class.getCanonicalName())));
+		assertEquals("Not cut away.", condensedText.getCodensedText());
+		assertEquals(0, condensedText.getCondensedOffsetForOriginalOffset(10));
+		assertEquals(3, condensedText.getCondensedOffsetForOriginalOffset(13));
+		assertEquals(13, condensedText.getCondensedOffsetForOriginalOffset(27));
+		assertEquals(13, condensedText.getCondensedOffsetForOriginalOffset(33));
 	}
 }
